47022356c14f98b5ccea194074baf81f9ecffbf8
kuzzmak <kuzminski.antonio@gmail.com>
2020-03-30 19:56:55 +0200

    updated functions

--- zadace/hw02-0036503800/src/main/java/hr/fer/zemris/java/custom/collections/ArrayIndexedCollection.java
+++ zadace/hw02-0036503800/src/main/java/hr/fer/zemris/java/custom/collections/ArrayIndexedCollection.java
@@ -66,6 +66,10 @@ public class ArrayIndexedCollection extends Collection {
 		}
 	}
 	
+	void addAll(Collection other) {
+		
+	}
+	
 	public Object[] toArray() {
 		
 		return new Object[] {this.getElements()};
@@ -75,8 +79,102 @@ public class ArrayIndexedCollection extends Collection {
 		return this.size;
 	}
 	
+	public boolean isEmpty() {
+		return this.size() == 0;
+	}
+	
+	boolean contains(Object value) {
+		
+		for(Object o: this.elements) {
+			if(o.equals(value)) return true;
+		}
+		
+		return false;
+	}
+	
+	boolean remove(Object value) {
+		
+		Object[] temp = new Object[this.elements.length];
+		
+		int index = 0;
+		boolean removed = false;
+		
+		for(int i = 0; i < this.size; i++) {
+			if(this.elements[i].equals(value)) {
+				index = i;
+				removed = true;
+				break;
+			}else {
+				temp[i] = this.elements[i];
+			}
+			
+		}
+		
+		for(int i = index + 1; i < this.size; i++) {
+			temp[i] = this.elements[i];
+		}
+		
+		return removed;
+	}
+	
 	public Object[] getElements() {
 		return this.elements;
+	}
+	
+	/**
+	 * Metoda za dohvat elementa polja na indeksu <code>index</code>.
+	 * 
+	 * @param index indeks na kojem se pokušava dohvatiti element
+	 * @throws IndexOutOfBoundsException ako je predani <code>index</code> izvan granica polja
+	 * @return element polja na mjestu <code>index</code>
+	 */
+	public Object get(int index) {
+		
+		if(index > this.size - 1 || index < 0) 
+			throw new IndexOutOfBoundsException("Nemoguće dohvatiti element izvan indeksa polja.");
+		
+		return this.elements[index];
+	}
+	
+	/**
+	 * Metoda za brisanje elemenata kolekcije. Polje unutar kolekcije ostaje jednake
+	 * veličine, ali su mu svi elementi <code>null</code>.
+	 * 
+	 */
+	void clear() {
+		
+		for(int i = 0; i < this.size; i++) {
+			this.elements[i] = null;
+		}
+		
+		this.size = 0;
+	}
+	
+	
+	void insert(Object value, int position) {
+		
+		if(value == null) 
+			throw new NullPointerException("Nije moguće dodati null u kolekciju.");
+		if(position < 0 || position > this.size) 
+			throw new IndexOutOfBoundsException("Neispravni indeksi za umetanje elementa.");
+			
+		
+	}
+	
+	/**
+	 * Metoda za pronalazak indeksa predanog elementa <code>value</code>.
+	 * Ukoliko predani elemnt nije član kolekcije, vraća se vrijednost -1. 
+	 * 
+	 * @param value element čiji se indeks pokušava naći
+	 * @return indeks traženog elementa ako je unutar kolekcije, -1 inače
+	 */
+	int indexOf(Object value) {
+		
+		for(int i = 0; i < this.size; i++) {
+			if(this.elements[i].equals(value)) return i;
+		}
+		
+		return -1;
 	}
 	
 }
