7e09a2311f740baa20adcf95cab729358414170c
kuzzmak <kuzminski.antonio@gmail.com>
2020-04-05 08:23:56 +0200

    updated processor class

--- zadace/hw02-0036503800/src/main/java/hr/fer/zemris/java/custom/collections/Processor.java
+++ zadace/hw02-0036503800/src/main/java/hr/fer/zemris/java/custom/collections/Processor.java
@@ -1,7 +1,18 @@
 package hr.fer.zemris.java.custom.collections;
 
+/**
+ * Model razreda koji ima mogućnost izvršavanja neke zadaće nad predanim objektom.
+ * 
+ * @author Antonio Kuzminski
+ *
+ */
 public class Processor {
 
+	/**
+	 * Metoda koja obavlja nekakvu zadaću nad predanim objektom.
+	 * 
+	 * @param value objekt nad kojim se izvršava neka zadaća
+	 */
 	public void process(Object value) {
 		
 	}
