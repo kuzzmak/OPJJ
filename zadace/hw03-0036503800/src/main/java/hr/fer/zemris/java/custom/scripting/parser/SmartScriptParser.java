405fac64550930d8720e4428f55c442049be4838
kuzzmak <kuzminski.antonio@gmail.com>
2020-04-10 22:23:49 +0200

    for parsing updated

--- zadace/hw03-0036503800/src/main/java/hr/fer/zemris/java/custom/scripting/parser/SmartScriptParser.java
+++ zadace/hw03-0036503800/src/main/java/hr/fer/zemris/java/custom/scripting/parser/SmartScriptParser.java
@@ -1,10 +1,11 @@
 package hr.fer.zemris.java.custom.scripting.parser;
 
 import hr.fer.zemris.java.custom.collections.ArrayIndexedCollection;
-import hr.fer.zemris.java.custom.collections.ElementsGetter;
 import hr.fer.zemris.java.custom.collections.ObjectStack;
-import hr.fer.zemris.java.custom.collections.Processor;
 import hr.fer.zemris.java.custom.scripting.elems.Element;
+import hr.fer.zemris.java.custom.scripting.elems.ElementConstantDouble;
+import hr.fer.zemris.java.custom.scripting.elems.ElementConstantInteger;
+import hr.fer.zemris.java.custom.scripting.elems.ElementString;
 import hr.fer.zemris.java.custom.scripting.elems.ElementVariable;
 import hr.fer.zemris.java.custom.scripting.lexer.SmartScriptLexer;
 import hr.fer.zemris.java.custom.scripting.lexer.Token;
@@ -19,7 +20,7 @@ public class SmartScriptParser {
 	private DocumentNode documentNode;
 
 	private ArrayIndexedCollection tokens;
-	
+
 	// index trenutnog tokena
 	private int index = 0;
 
@@ -48,52 +49,213 @@ public class SmartScriptParser {
 
 		boolean inTag = false;
 		boolean textNodeCreated = false;
-		
-		while(index < tokens.size()) {
-			
+
+		while (index < tokens.size()) {
+
 			Object value = tokens.get(index);
-			
+
 			if (value == null)
 				throw new NullPointerException("Nije moguće predati null na obradu.");
 
 			if (!(value instanceof Token))
 				throw new IllegalArgumentException("Nije moguće obraditi objekte koji nisu Token.");
-			
-			Token token = (Token)value;
-			
+
+			Token token = (Token) value;
+
 			// token za početak taga
-			if(token.getType() == TokenType.TAG_START) {
-				
-				if(index + 1 >= tokens.size())
+			if (token.getType() == TokenType.TAG_START) {
+
+				if (index + 1 >= tokens.size())
 					throw new SmartScriptParserException("Početak taga ne može bit na kraju dokumenta.");
-				
-				String tagName = (String)((Token)tokens.get(index + 1)).getValue();
-				
-				
-				if(tagName.equals("FOR")) {
-					
+
+				String tagName = (String) ((Token) tokens.get(index + 1)).getValue();
+
+				if (tagName.equals("FOR")) {
+
 					// pozicija FOR tokena
 					index++;
 					Node node = extractFOR();
-					
-				}else if(tagName.equals("END")) {
-					
-				}else if(tagName.equals("=")) {
-					
-				}else throw new SmartScriptParserException("Neispravan tag.");
+
+				} else if (tagName.equals("END")) {
+
+				} else if (tagName.equals("=")) {
+
+				} else
+					throw new SmartScriptParserException("Neispravan tag.");
+
+			} else {
+
+				TextNode textNode = new TextNode((String) token.getValue());
+				currentNode.addChild(textNode);
+				index++;
 			}
 		}
 	}
-	
+
+	/**
+	 * Metoda za parsiranje tokena u ForLoopNode node.
+	 * 
+	 * @throws SmartScriptParserException ako je došlo do kakve greške 
+	 *         prilikom parsiranja
+	 * @return objekt tipa ForLoopNode 
+	 */
 	public ForLoopNode extractFOR() {
+
+		// tag poslije imena FOR
+		index++;
+		Token token = (Token) tokens.get(index);
+		// brojač parametara for petlje
+		int counter = 0;
+
+		ElementVariable variable = null;
+		Element startExpression = null;
+		Element endExpression = null;
+		Element stepExpression = null;
+
+		// sve do kraja taga
+		while (index < tokens.size() && token.getValue() != TokenType.TAG_END) {
+
+			token = (Token) tokens.get(index);
+			
+			if(counter == 3) { // ako su svi parametri FOR popunjeni, 
+							   // a nije token za kraj
+				
+				boolean end = token.getType() == TokenType.TAG_END;
+				
+				if(!end)
+					throw new SmartScriptParserException("Previše argumenata u FOR.");
+			}
+			
+			if(token.getType() == TokenType.TAG_END) { // kraj FOR
+				index++; // prelazak na sljedeći tag ako postoji
+				break;
+			}
+
+			// treba uzeti varijablu, prva iteracije while petlje
+			if (counter == 0) {
+
+				boolean ok = checkVarable(token);
+
+				if (ok) {
+					variable = new ElementVariable((String) token.getValue());
+					counter++;
+					index++;
+				} else
+					throw new SmartScriptParserException("Neispravno ime varijable.");
+
+			} else if (counter == 1) { // popunjava se startExpression
+
+				startExpression = extractElement();
+				
+				if(startExpression == null)
+					throw new SmartScriptParserException("Greška prilikom parsiranja FOR.");
+				
+				counter++;
+				continue;
+
+			}else if(counter == 2) {
+				
+				endExpression = extractElement();
+				
+				if(endExpression == null)
+					throw new SmartScriptParserException("Greška prilikom parsiranja FOR.");
+				
+				counter++;
+				continue;
+				
+			}else if(counter == 3) {
+				
+				stepExpression = extractElement();
+				counter++;
+				continue;
+			}
+		}
 		
-		ElementVariable variable;
-		Element startExpression;
-		Element endExpression;
-		Element stepExpression; 
-		
-		
-		
-		
+		if(counter < 2)
+			throw new SmartScriptParserException("Premalo argumenata u FOR.");
+
+		return new ForLoopNode(variable, startExpression, endExpression, stepExpression);
+	}
+
+	public Element extractElement() {
+
+		Token token = (Token) tokens.get(index);
+
+		Element expression = null;
+
+		if (token.getType() == TokenType.QUOTEMARK) { // konstanta kao string
+
+			index++; // pomak do broja unutar navodnika
+
+			token = (Token) tokens.get(index);
+
+			expression = new ElementString((String) token.getValue());
+
+			index++; // navodnici nakon broja
+			index++; // token nakon navodnika
+
+		} else if (token.getType() == TokenType.NUMBER) {
+
+			String number = String.valueOf(token.getValue());
+
+			if (number.contains(".")) { // ako je decimalni broj
+
+				try {
+
+					double num = Double.parseDouble(number);
+					expression = new ElementConstantDouble(num);
+					index++;
+
+				} catch (NumberFormatException e) {
+					throw new SmartScriptParserException("Nije moguće parsirati broj.");
+				}
+			} else { // integer
+
+				try {
+
+					int num = Integer.parseInt(number);
+					expression = new ElementConstantInteger(num);
+					index++;
+
+				} catch (NumberFormatException e) {
+					throw new SmartScriptParserException("Nije moguće parsirati broj.");
+				}
+			}
+		}
+
+		return expression;
+	}
+
+	/**
+	 * Funkcija za provjeravanje je li ime varijable ispravno.
+	 * 
+	 * @param token token koji sadrži ime varijable
+	 * @return istina ako je u redu, laž inače
+	 */
+	public boolean checkVarable(Token token) {
+
+		String variable = String.valueOf(token.getValue());
+
+		char[] symbols = variable.toCharArray();
+
+		// ako ne počinje slovom, ne valja
+		if (!Character.isLetter(symbols[0]))
+			return false;
+
+		// ostatak simbola imena ako postoje
+		if (symbols.length > 1) {
+
+			for (int i = 1; i < symbols.length; i++) {
+
+				boolean ok = Character.isLetter(symbols[i]) || 
+						Character.isDigit(symbols[i]) || 
+						symbols[i] == '_';
+
+				if (!ok)
+					return false;
+			}
+		}
+
+		return true;
 	}
 }
