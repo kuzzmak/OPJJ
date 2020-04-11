fcdcbf33967502f15cff801583cc14ae6556a82c
kuzzmak <kuzminski.antonio@gmail.com>
2020-04-11 16:09:32 +0200

    added tests

--- zadace/hw03-0036503800/src/main/java/hr/fer/zemris/java/custom/scripting/parser/test.java
+++ zadace/hw03-0036503800/src/main/java/hr/fer/zemris/java/custom/scripting/parser/test.java
@@ -6,6 +6,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
+import hr.fer.zemris.java.custom.scripting.nodes.DocumentNode;
+import hr.fer.zemris.java.custom.scripting.nodes.TextNode;
+
 public class test {
 	
 	private static String loader(String fileName) {
@@ -30,27 +33,38 @@ public class test {
 	public static void main(String[] args) throws IOException {
 		
 		String document = loader("src/test/resources/document1.txt");
-//		
+		
+		String primjer = loader("src/test/resources/extra/primjer8.txt");
+		
 		String error1 = "{$ FOR 3 1 10 1 $}"; //OK
 		String error2 = "{$ FOR * \"1\" -10 \"1\" $}"; //OK
 		String error3 = "{$ FOR year @sin 10 $}"; //OK
 		String error4 = "{$ FOR year 1 10 \"1\" \"10\" $}"; //OK
 		String error5 = "{$ FOR year $}"; //OK
 		String error6 = "{$ FOR year 1 10 1 3 $}"; //OK
-//		
+		
 		String for1 = "{$ FOR i-1.35bbb\"1\" $}"; //OK
 		String for2 = "{$ FOR i -1.35 bbb   $}"; //OK
+		String for3 = "{$ FOR year 1 last_year $}";
 		
 		String tekst1 = "Example { bla } blu \\{$=1$} Nothing interesting {=here}"; //OK
 		String tekst2 = "A tag follows {$= \"Joe \\\"Long\\\" Smith\"$}"; //OK
+		String tekst3 = "Example \\{$=1$}. Now actually write one {$=1$}";
 		
-		String equals1 = "{$= i i * @sin \"0.000\" @decfmt $}";
+		String equals1 = "{$= i i * @sin \"0.000\" @decfmt $}"; //OK
 		
-		SmartScriptParser parser = new SmartScriptParser(equals1);
 		
-//		Object o = null;
-//		
-//		System.out.println(o == null ? "null": "nije null");
+		String foor = "sin({$=i$}^2) = {$= i i * @sin \"0.000\" @decfmt $}";
 		
+		SmartScriptParser parser1 = new SmartScriptParser(document);
+		DocumentNode documentNode1 = parser1.getDocmentNode();
+		System.out.println(documentNode1);
+		System.out.println("-------------------------");
+		System.out.println();
+		
+		SmartScriptParser parser2 = new SmartScriptParser(documentNode1.toString());
+		DocumentNode documentNode2 = parser2.getDocmentNode();
+		
+		System.out.println(documentNode2);
 	}
 }
