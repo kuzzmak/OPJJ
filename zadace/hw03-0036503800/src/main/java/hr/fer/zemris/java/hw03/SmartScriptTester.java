f4c4ac408086009b42b9c38cdf5bbb763cea035f
kuzzmak <kuzminski.antonio@gmail.com>
2020-04-11 22:22:15 +0200

    demo for parser

---
+++ zadace/hw03-0036503800/src/main/java/hr/fer/zemris/java/hw03/SmartScriptTester.java
@@ -0,0 +1,44 @@
+package hr.fer.zemris.java.hw03;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import hr.fer.zemris.java.custom.scripting.lexer.SmartScriptingLexerException;
+import hr.fer.zemris.java.custom.scripting.nodes.DocumentNode;
+import hr.fer.zemris.java.custom.scripting.parser.SmartScriptParser;
+
+public class SmartScriptTester {
+
+	public static void main(String[] args) throws IOException {
+		
+//		String docBody = loader("src/test/resources/document1.txt");
+		
+		if(args.length != 1)
+			throw new IllegalArgumentException("Broj argumenata mora biti 1 - staza do datoteke koja se parsira.");
+		
+		String filePath = args[0];
+		
+		String docBody = new String(Files.readAllBytes(Paths.get(filePath)), StandardCharsets.UTF_8);
+		
+		SmartScriptParser parser = null;
+		
+		try {
+			parser = new SmartScriptParser(docBody);
+		}catch(SmartScriptingLexerException e) {
+			System.out.println("Unable to parse document.");
+			System.exit(-1);
+		}catch(Exception e) {
+			System.out.println("If this line ever executes, you have failed this class.");
+			System.exit(-1);
+		}
+		
+		DocumentNode document = parser.getDocmentNode();
+		String originalDocumentBody = document.toString();
+		System.out.println(originalDocumentBody);
+	}
+}
