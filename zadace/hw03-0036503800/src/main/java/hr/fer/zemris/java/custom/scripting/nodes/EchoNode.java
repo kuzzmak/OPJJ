c0054fa5866c1dab1026ab8f46f61bc9363db7ad
kuzzmak <kuzminski.antonio@gmail.com>
2020-04-11 11:13:48 +0200

    added ability to add elements to private array

--- zadace/hw03-0036503800/src/main/java/hr/fer/zemris/java/custom/scripting/nodes/EchoNode.java
+++ zadace/hw03-0036503800/src/main/java/hr/fer/zemris/java/custom/scripting/nodes/EchoNode.java
@@ -1,8 +1,38 @@
 package hr.fer.zemris.java.custom.scripting.nodes;
 
+import hr.fer.zemris.java.custom.collections.ArrayIndexedCollection;
 import hr.fer.zemris.java.custom.scripting.elems.Element;
 
 public class EchoNode extends Node {
 
 	Element[] elements;
+	
+	ArrayIndexedCollection col;
+	
+	public EchoNode() {
+		
+		this.col = new ArrayIndexedCollection();
+	}
+	
+	public void addElement(Element element) {
+		
+		if(element == null)
+			throw new NullPointerException("Nije moguće dodati null kao element.");
+		
+		if(!(element instanceof Element))
+			throw new IllegalArgumentException("Nije moguće dodati objekt koji nije tipa Element.");
+		
+		col.add(element);
+	}
+	
+	public Element[] getElements() {
+		
+		elements = new Element[col.size()];
+		
+		for(int i = 0; i < col.size(); i++) {
+			elements[i] = (Element)col.get(i);
+		}
+		
+		return elements;
+	}
 }
