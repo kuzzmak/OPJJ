856ca33f707b41170d6b207bb3e23f2b85d7ea73
kuzzmak <kuzminski.antonio@gmail.com>
2020-04-15 11:34:10 +0200

    updated

--- zadace/hw04-0036503800/src/main/java/hr/fer/zemris/java/custom/collections/SimpleHashTable.java
+++ zadace/hw04-0036503800/src/main/java/hr/fer/zemris/java/custom/collections/SimpleHashTable.java
@@ -2,6 +2,15 @@ package hr.fer.zemris.java.custom.collections;
 
 public class SimpleHashTable<K, V> {
 
+	/**
+	 * Interni razred koji ima ulogu jednog uređenog para {@code (K, V)}
+	 * gdje {@code K} ima ulogu ključa, a {@code V} vrijednosti pod tim ključem.
+	 * 
+	 * @author Antonio Kuzminski
+	 *
+	 * @param <K> ključ
+	 * @param <V> vrijednost
+	 */
 	private static class TableEntry<K, V>{
 		
 		private K key;
@@ -27,7 +36,7 @@ public class SimpleHashTable<K, V> {
 
 		@Override
 		public String toString() {
-			return "(" + key + ", " + value + ")";
+			return key + "=" + value;
 		}
 	}
 	
@@ -77,7 +86,7 @@ public class SimpleHashTable<K, V> {
 		if(key == null)
 			throw new NullPointerException("Ključ ne može biti null.");
 		
-		int slot = Math.abs(key.hashCode()) % entries.length;;
+		int slot = getSlot(key);
 		
 		if(entries[slot] != null) {
 			
@@ -116,7 +125,7 @@ public class SimpleHashTable<K, V> {
 		
 		if(key == null) return null;
 		
-		int slot = Math.abs(key.hashCode()) % entries.length;
+		int slot = getSlot(key);
 		
 		TableEntry<K, V> entry = entries[slot];
 		
@@ -138,11 +147,17 @@ public class SimpleHashTable<K, V> {
 		return size;
 	}
 	
+	/**
+	 * Metoda za provjeru sadrži li tablica određen ključ.
+	 * 
+	 * @param key ključ za koji se provjerava je li sadržan u tablici
+	 * @return istina ako ključ <code>key</code> postoji, laž inače
+	 */
 	public boolean containsKey(Object key) {
 		
 		if(key == null) return false;
 		
-		int slot = Math.abs(key.hashCode()) % entries.length;
+		int slot = getSlot(key);
 		
 		TableEntry<K, V> entry = entries[slot];
 		
@@ -154,20 +169,125 @@ public class SimpleHashTable<K, V> {
 		return false;
 	}
 	
+	/**
+	 * Metoda za provjeru sadrži li tablica vrijednost {@code value}.
+	 * 
+	 * @param value vrijednost za koju se provjerava je li u tablici
+	 * @return istina ako je {@code value} sadržana u tablici, laž inače
+	 */
 	public boolean containsValue(Object value) {
+		
+		for(int i = 0; i < entries.length; i++) {
+			
+			if(entries[i] == null) continue;
+			else {
+				
+				TableEntry<K, V> entry = entries[i];
+				
+				while(entry != null) {
+					
+					if(value != null) {
+						if(entry.getValue().equals(value)) return true;
+					}else {
+						if(entry.getValue() == null) return true;
+					}
+					
+					entry = entry.next;
+				}
+			}
+		}
 		return false;
 	}
 	
+	/**
+	 * Metoda za uklanjanje uređenog para {@code (K, V)} iz tablice pod
+	 * ključem {@code K} ako on postoji.
+	 * 
+	 * @param key ključ čiji se par uklanja iz tablice
+	 * @throws NullPointerException ako je predani ključ {@code key null} 
+	 */
 	public void remove(Object key) {
 		
+		if(key == null)
+			throw new NullPointerException("Nije moguće izbrisati null jer ne postoji.");
+		
+		int slot = getSlot(key);
+		
+		TableEntry<K, V> entry = entries[slot];
+		
+		// ako je prazan slot
+		if(entry == null) return;
+		
+		// ako je samo jedan entry u slotu
+		if(entry.next == null) {
+			entries[slot] = null;
+			size--;
+			return;
+		}
+		
+		TableEntry<K, V> previousEntry = entry;
+		
+		while(entry != null) {
+			
+			if(entry.getKey().equals(key)) {
+				
+				previousEntry.next = entry.next;
+				entry = null;
+				size--;
+				return;
+			}
+			
+			previousEntry = entry;
+			entry = entry.next;
+		}
 	}
 	
+	/**
+	 * Metoda za provjeru je li tablica prazna.
+	 * 
+	 * @return istina ako je tablica prazna,  laž inače
+	 */
 	public boolean isEmpty() {
-		return false;
+		return size == 0;
+	}
+	
+	/**
+	 * Funkcija za izračun mjesta u tablici u koje se novi par
+	 * {@code (K, V)} mora ubaciti.
+	 * 
+	 * @param key ključ za koji se mjesto izračunava
+	 * @return redni broj mjesta
+	 */
+	public int getSlot(Object key) {
+		return Math.abs(key.hashCode()) % entries.length;
 	}
 
 	@Override
 	public String toString() {
-		return "SimpleHashTable []";
+		
+		if(isEmpty()) return "[]";
+		else {
+			
+			StringBuilder sb = new StringBuilder();
+			sb.append("[");
+			
+			for(int i = 0; i < entries.length; i++) {
+				
+				if(entries[i] != null) {
+					
+					TableEntry<K, V> entry = entries[i];
+					
+					while(entry != null) {
+						
+						sb.append(entry).append(", ");
+						entry = entry.next;
+					}
+				}
+			}
+			
+			sb.replace(sb.length() - 2, sb.length(), "");
+			sb.append("]");
+			return sb.toString();
+		}
 	}
 }
