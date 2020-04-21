55c1c20d134bac18f71ed0a5bbd68bd9a8e95d2e
kuzzmak <kuzminski.antonio@gmail.com>
2020-04-21 14:12:04 +0200

    updated javadoc

--- zadace/hw05-0036503800/src/main/java/hr/fer/zemris/lsystems/impl/Command.java
+++ zadace/hw05-0036503800/src/main/java/hr/fer/zemris/lsystems/impl/Command.java
@@ -2,7 +2,19 @@ package hr.fer.zemris.lsystems.impl;
 
 import hr.fer.zemris.lsystems.Painter;
 
+/**
+ * Sučelje koje modelira neku vrstu naredbe. Svaka naredba se može izvršiti.
+ * 
+ * @author Antonio Kuzminski
+ *
+ */
 public interface Command {
 
+	/**
+	 * Metoda koja izvršava dotičnu neredbu.
+	 * 
+	 * @param ctx kontekst nad kojim se naredba izvršava
+	 * @param painter objekt nad kojim se crta
+	 */
 	void execute(Context ctx, Painter painter);
 }
