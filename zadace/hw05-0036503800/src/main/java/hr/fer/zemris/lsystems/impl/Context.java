11c596b6d878bf0d5f16ecc952a70ddc4d17dec8
kuzzmak <kuzminski.antonio@gmail.com>
2020-04-21 10:52:47 +0200

    updated functions

--- zadace/hw05-0036503800/src/main/java/hr/fer/zemris/lsystems/impl/Context.java
+++ zadace/hw05-0036503800/src/main/java/hr/fer/zemris/lsystems/impl/Context.java
@@ -1,16 +1,50 @@
 package hr.fer.zemris.lsystems.impl;
 
-public class Context {
+import hr.fer.zemris.java.custom.collections.ObjectStack;
 
+/**
+ * Razred koji služi za izvođenje postupka prikazivanja fraktala
+ * i stavljanje te dohvaćanje stanja kornjače.
+ * 
+ * @author Antonio Kuzminski
+ *
+ */
+public class Context {
+	
+	private ObjectStack<TurtleState> contextStack;
+	
+	/**
+	 * Inicijalni konstruktor.
+	 * 
+	 */
+	public Context() {
+		contextStack = new ObjectStack<>();
+	}
+
+	/**
+	 * Dohvat trenutnog stanja kornjače.
+	 * 
+	 * @return trenutno stanje kornjače.
+	 */
 	TurtleState getCurrentState() {
-		return null;
+		return contextStack.peek();
 	}
 	
-	void pushState() {
-		
+
+	/**
+	 * Stavljanje trenutnoh stanje kornjače na stog.
+	 * 
+	 * @param state stanje koje se stavlja na stog
+	 */
+	void pushState(TurtleState state) {
+		contextStack.push(state);
 	}
 	
+	/**
+	 * Micanje zadnjeg stanja na stogu.
+	 * 
+	 */
 	void popState() {
-		
+		contextStack.pop();
 	}
 }
