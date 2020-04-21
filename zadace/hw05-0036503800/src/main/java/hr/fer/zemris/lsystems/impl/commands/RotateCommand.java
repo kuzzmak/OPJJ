0347084de704c373d3005d5e96dd2780c91165f2
kuzzmak <kuzminski.antonio@gmail.com>
2020-04-21 19:37:39 +0200

    updated function for rotation

--- zadace/hw05-0036503800/src/main/java/hr/fer/zemris/lsystems/impl/commands/RotateCommand.java
+++ zadace/hw05-0036503800/src/main/java/hr/fer/zemris/lsystems/impl/commands/RotateCommand.java
@@ -3,17 +3,32 @@ package hr.fer.zemris.lsystems.impl.commands;
 import hr.fer.zemris.lsystems.Painter;
 import hr.fer.zemris.lsystems.impl.Command;
 import hr.fer.zemris.lsystems.impl.Context;
+import hr.fer.zemris.math.Vector2D;
 
+/**
+ * Naredba za rotaciju kornjače.
+ * 
+ * @author Antonio Kuzminski
+ *
+ */
 public class RotateCommand implements Command {
 	
+	private double angle;
+	
+	/**
+	 * Inicijalni konstruktor.
+	 * 
+	 * @param angle kut zakreta kornjače u stupnjevima
+	 */
 	public RotateCommand(double angle) {
-		// TODO Auto-generated constructor stub
+		this.angle = angle;
 	}
 
 	@Override
 	public void execute(Context ctx, Painter painter) {
-		// TODO Auto-generated method stub
-
+		
+		Vector2D currentDirection = ctx.getCurrentState().getDirection();
+		
+		ctx.getCurrentState().setDirection(currentDirection.rotated(angle));
 	}
-
 }
