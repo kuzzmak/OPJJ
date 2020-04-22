168246e237688157bf582e4e1aa3454590782ba5
kuzzmak <kuzminski.antonio@gmail.com>
2020-04-22 17:44:41 +0200

    added student record

---
+++ zadace/hw05-0036503800/src/main/java/hr/fer/zemris/java/hw05/db/StudentRecord.java
@@ -0,0 +1,56 @@
+package hr.fer.zemris.java.hw05.db;
+
+/**
+ * Razred predstavlja jedan zapis o studentu u bazi podataka. Studenti
+ * se razlikuju prema jmbag-u.
+ * 
+ * @author Antonio Kuzminski
+ *
+ */
+public class StudentRecord {
+
+	private String jmbag;
+	private String firstName;
+	private String lastName;
+	private int finalGrade;
+	/**
+	 * Inicijalni konstruktor.
+	 * 
+	 * @param jmbag identifikacijski broj studenta
+	 * @param firstName ime studenta
+	 * @param lastName prezime studenta
+	 * @param finalGrade zaklkjučena ocjena studenta
+	 */
+	public StudentRecord(String jmbag, String firstName, String lastName, int finalGrade) {
+		super();
+		this.jmbag = jmbag;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.finalGrade = finalGrade;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((jmbag == null) ? 0 : jmbag.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		StudentRecord other = (StudentRecord) obj;
+		if (jmbag == null) {
+			if (other.jmbag != null)
+				return false;
+		} else if (!jmbag.equals(other.jmbag))
+			return false;
+		return true;
+	}
+}
