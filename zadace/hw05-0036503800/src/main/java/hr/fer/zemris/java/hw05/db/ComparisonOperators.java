ad922bd92b1c177e15755fed76fb225d7d8b6f9f
kuzzmak <kuzminski.antonio@gmail.com>
2020-04-22 21:31:13 +0200

    added LIKE comparison operator

--- zadace/hw05-0036503800/src/main/java/hr/fer/zemris/java/hw05/db/ComparisonOperators.java
+++ zadace/hw05-0036503800/src/main/java/hr/fer/zemris/java/hw05/db/ComparisonOperators.java
@@ -19,4 +19,27 @@ public class ComparisonOperators{
 	public static final IComparisonOperator EQUALS = (s1, s2) -> s1.compareTo(s2) == 0;
 	
 	public static final IComparisonOperator NOT_EQUALS = (s1, s2) -> s1.compareTo(s2) != 0;
+	
+	public static final IComparisonOperator LIKE = new IComparisonOperator() {
+		
+		@Override
+		public boolean satisfied(String value1, String value2) {
+			
+			for(int i = 0, j = 0; i < value2.length() && j < value2.length();) {
+				
+				if(i > value1.length() - 1) return false; // kada je prvi string kraći od drugog
+				
+				if(value2.charAt(j) == '*') {
+					j++;
+					continue;
+				}
+				
+				if(value2.charAt(j) != value1.charAt(i)) 
+					return false;
+				i++;
+				j++;
+			}
+			return true;
+		}
+	};
 }
