02fa7a8e27a2675a868fa5ed31094ee8e29d2261
kuzzmak <kuzminski.antonio@gmail.com>
2020-04-22 13:53:39 +0200

    implemented

--- zadace/hw05-0036503800/src/main/java/hr/fer/zemris/lsystems/impl/commands/ScaleCommand.java
+++ zadace/hw05-0036503800/src/main/java/hr/fer/zemris/lsystems/impl/commands/ScaleCommand.java
@@ -4,16 +4,26 @@ import hr.fer.zemris.lsystems.Painter;
 import hr.fer.zemris.lsystems.impl.Command;
 import hr.fer.zemris.lsystems.impl.Context;
 
+/**
+ * Naredba za promjenu veličine trenutnog koraka na način 
+ * da se pomnoži nekim faktorom.
+ * 
+ * @author Antonio Kuzminski
+ *
+ */
 public class ScaleCommand implements Command {
 
+	private double scale;
+	
 	public ScaleCommand(double scale) {
-		
+		this.scale = scale;
 	}
 	
 	@Override
 	public void execute(Context ctx, Painter painter) {
-		// TODO Auto-generated method stub
-
+		
+		double currentStep = ctx.getCurrentState().getStep();
+		
+		ctx.getCurrentState().setStep(scale * currentStep);
 	}
-
 }
