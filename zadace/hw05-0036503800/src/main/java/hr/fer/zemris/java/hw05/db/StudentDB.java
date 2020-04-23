de991f38a444b6f9c39d4a0a23c9fb810431e9aa
kuzzmak <kuzminski.antonio@gmail.com>
2020-04-23 13:59:44 +0200

    added functionality

--- zadace/hw05-0036503800/src/main/java/hr/fer/zemris/java/hw05/db/StudentDB.java
+++ zadace/hw05-0036503800/src/main/java/hr/fer/zemris/java/hw05/db/StudentDB.java
@@ -5,46 +5,87 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
+/**
+ * Razred koji oponaša jednostvnu bazu podataka.
+ * 
+ * @author Antonio Kuzminski
+ *
+ */
 public class StudentDB {
-	
+
 	Path databaseFile = Paths.get("src/test/resources/database.txt");
-	
+
 	StudentDatabase sdb;
 
+	/**
+	 * Inicijalni konstruktor.
+	 * 
+	 */
 	public StudentDB() {
-		
+
 		List<String> lines = null;
-		
+
 		try {
 			lines = Files.readAllLines(databaseFile, Charset.defaultCharset());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		sdb = new StudentDatabase(lines);
 	}
-	
-	public void query(String query) {
-		
+
+	/**
+	 * Metoda za izvršavanje pojedinog upita.
+	 * 
+	 * @param query upit koji je potrebno izvršiti
+	 */
+	private void query(String query) {
+
 		QueryParser qp = new QueryParser(query);
-		
-		if(qp.isDirectQuery()) {
+
+		if (qp.isDirectQuery()) {
 			
-			System.out.println(sdb.forJMBAG(qp.getQueriedJMBAG()));
-			
-		}else {
-			
-			for(StudentRecord sr: sdb.filter(new QueryFilter(qp.getQuerry()))) {
-				
-				
-				System.out.println(sr);
-				
+			RecordFormatter
+			.format(
+					new ArrayList<StudentRecord>(
+							Arrays.asList(sdb.forJMBAG(qp.getQueriedJMBAG()))))
+			.forEach(System.out::println);
+
+		} else {
+
+			List<StudentRecord> records = new ArrayList<>();
+
+			for (StudentRecord sr : sdb.filter(new QueryFilter(qp.getQuerry()))) {
+
+				records.add(sr);
 			}
-			
+
+			RecordFormatter.format(records).forEach(System.out::println);
 		}
-		
-		
+	}
+
+	public static void main(String[] args) {
+
+		StudentDB db = new StudentDB();
+
+		try (Scanner sc = new Scanner(System.in)) {
+
+			while (sc.hasNextLine()) {
+
+				String line = sc.nextLine().strip();
+
+				if (line.equals("exit")) {
+					System.out.println("Goodbye!");
+					System.exit(0);
+				}
+
+				db.query(line);
+			}
+		}
 	}
 }
