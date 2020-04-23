a83e9ae9bfb8e1c0d40ff2e7eca366a023d6cf95
kuzzmak <kuzminski.antonio@gmail.com>
2020-04-23 10:59:55 +0200

    bug fix

--- zadace/hw05-0036503800/src/main/java/hr/fer/zemris/java/hw05/db/ComparisonOperators.java
+++ zadace/hw05-0036503800/src/main/java/hr/fer/zemris/java/hw05/db/ComparisonOperators.java
@@ -6,38 +6,51 @@ package hr.fer.zemris.java.hw05.db;
  * @author Antonio Kuzminski
  *
  */
-public class ComparisonOperators{
+public class ComparisonOperators {
 
 	public static final IComparisonOperator LESS = (s1, s2) -> s1.compareTo(s2) < 0;
-	
+
 	public static final IComparisonOperator LESS_OR_EQUALS = (s1, s2) -> s1.compareTo(s2) <= 0;
 
 	public static final IComparisonOperator GREATER = (s1, s2) -> s1.compareTo(s2) > 0;
-	
+
 	public static final IComparisonOperator GREATER_OR_EQUALS = (s1, s2) -> s1.compareTo(s2) >= 0;
 
 	public static final IComparisonOperator EQUALS = (s1, s2) -> s1.compareTo(s2) == 0;
-	
+
 	public static final IComparisonOperator NOT_EQUALS = (s1, s2) -> s1.compareTo(s2) != 0;
-	
+
 	public static final IComparisonOperator LIKE = new IComparisonOperator() {
-		
+
 		@Override
 		public boolean satisfied(String value1, String value2) {
-			
-			for(int i = 0, j = 0; i < value2.length() && j < value2.length();) {
-				
-				if(i > value1.length() - 1) return false; // kada je prvi string kraći od drugog
-				
-				if(value2.charAt(j) == '*') {
-					j++;
-					continue;
+
+			if (value2.length() - value1.length() >= 2)
+				return false;
+
+			int indexOfAsterisk = -1;
+
+			for (int i = 0; i < value2.length(); i++) {
+
+				if (value2.charAt(i) == '*') {
+					indexOfAsterisk = i;
+					break;
 				}
-				
-				if(value2.charAt(j) != value1.charAt(i)) 
+
+				if (value2.charAt(i) != value1.charAt(i))
 					return false;
-				i++;
-				j++;
+			}
+
+			String rest = new String(value1.toCharArray(), indexOfAsterisk, value1.length() - indexOfAsterisk);
+
+			for (int i = 0; i < rest.length(); i++) {
+
+				if (value2.charAt(value2.length() - 1 - i) == '*')
+					break;
+
+				if (value2.charAt(value2.length() - 1 - i) != value1.charAt(value1.length() - 1 - i))
+					return false;
+
 			}
 			return true;
 		}
