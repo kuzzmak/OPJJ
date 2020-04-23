5650be390c164f36e359975de477fff9ebd9e00f
kuzzmak <kuzminski.antonio@gmail.com>
2020-04-23 18:29:57 +0200

    added exceptions

--- zadace/hw05-0036503800/src/main/java/hr/fer/zemris/java/hw05/db/QueryParser.java
+++ zadace/hw05-0036503800/src/main/java/hr/fer/zemris/java/hw05/db/QueryParser.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import hr.fer.zemris.java.hw05.querylexer.QueryLexer;
+import hr.fer.zemris.java.hw05.querylexer.QueryLexerException;
 import hr.fer.zemris.java.hw05.querylexer.Token;
 import hr.fer.zemris.java.hw05.querylexer.TokenType;
 
@@ -16,38 +17,47 @@ import hr.fer.zemris.java.hw05.querylexer.TokenType;
 public class QueryParser {
 
 	List<Token> tokens;
-	
+
 	/**
 	 * Inicijalni konstruktor.
 	 * 
+	 * @throws QueryLexerException ako je došlo do greške prilikom leksičke analize
 	 * @param query string upita koji je potrebno parsirati
 	 */
 	public QueryParser(String query) {
-		
-		QueryLexer ql = new QueryLexer(query);
-		tokens = ql.tokenize();
+
+		QueryLexer ql;
+		try {
+			ql = new QueryLexer(query);
+			tokens = ql.tokenize();
+
+		} catch (QueryLexerException e) {
+			throw new QueryParserException(e.getMessage());
+		}
 	}
-	
+
 	/**
-	 * Metoda za provjeru je li trenutni upit "direct query".
-	 * Direct query je upit tipa jmbag="xxx".
+	 * Metoda za provjeru je li trenutni upit "direct query". Direct query je upit
+	 * tipa jmbag="xxx".
 	 * 
 	 * @return istina ako je trenutni upit "direct query", laž inače
 	 */
 	public boolean isDirectQuery() {
-		
-		// tokeni moraju biti identifikator jmbag, operator usporedbe, 
+
+		// tokeni moraju biti identifikator jmbag, operator usporedbe,
 		// neki jmbag i na kraju token EOF
-		if(tokens.size() != 4) return false;
-		
+		if (tokens.size() != 4)
+			return false;
+
 		boolean jmbag = tokens.get(0).getValue().equals("jmbag");
 		boolean operator = tokens.get(1).getValue().equals("=");
-		
-		if(jmbag && operator) return true;
-		
+
+		if (jmbag && operator)
+			return true;
+
 		return false;
 	}
-	
+
 	/**
 	 * Metoda za vraćanje jmbaga iz "direct query".
 	 * 
@@ -55,50 +65,71 @@ public class QueryParser {
 	 * @return jmgag ako je upit "direct query"
 	 */
 	public String getQueriedJMBAG() {
-		
-		if(!isDirectQuery()) 
+
+		if (!isDirectQuery())
 			throw new IllegalStateException("Upit nije direct query.");
-		
+
 		return String.valueOf(tokens.get(2).getValue());
 	}
-	
+
 	/**
 	 * Metoda za parsiranje tokena u upite.
 	 * 
+	 * @throws QueryParserException ako je došlo do greške prilikom parsiranja
 	 * @return lista upita tipa {@code ConditionalExpression}
 	 */
-	public List<ConditionalExpression> getQuerry(){
-		
+	public List<ConditionalExpression> getQuerry() {
+
 		List<ConditionalExpression> expressions = new ArrayList<>();
-		
-		for(int i = 0; i < tokens.size(); i+=3) {
-			
-			if(tokens.get(i).getType() == TokenType.EOF)
+
+		if (tokens.size() < 4)
+			throw new QueryParserException("Neispravan upit.");
+
+		for (int i = 0; i < tokens.size(); i += 3) {
+
+			if (tokens.get(i).getType() == TokenType.EOF)
 				return expressions;
-			
+
 			// logički operator koji povezuje više izraza
-			if(tokens.get(i).getType() == TokenType.LOGICAL_OPERATOR &&
-					String.valueOf(tokens.get(i).getValue()).toLowerCase().equals("and")) {
+			if (tokens.get(i).getType() == TokenType.LOGICAL_OPERATOR
+					&& String.valueOf(tokens.get(i).getValue()).toLowerCase().equals("and")) {
 				i -= 2;
 				continue;
 			}
-			
-			String atribute = String.valueOf(tokens.get(i).getValue()); 
-			IFieldValueGetter fieldValueGetter = getFiedlValueGetter(atribute);
-			
-			String stringLiteral = String.valueOf(tokens.get(i + 2).getValue());
-			
-			String operator = String.valueOf(tokens.get(i + 1).getValue());
-			IComparisonOperator comparisonOperator = getComparitsonOperator(operator);
-			
-			expressions.add(new ConditionalExpression(
-								fieldValueGetter, 
-								stringLiteral, 
-								comparisonOperator));
+
+			// ako je neki od sljedeća tri tokena EOF, neispravan upit
+			for (int j = 0; j < 3; j++) {
+				if (tokens.get(i + j).getType() == TokenType.EOF)
+					throw new QueryParserException("Neispravan upit.");
+			}
+
+			try {
+
+				String atribute = String.valueOf(tokens.get(i).getValue());
+
+				IFieldValueGetter fieldValueGetter = getFiedlValueGetter(atribute);
+
+				if (fieldValueGetter == null)
+					throw new QueryParserException("Neispravan upit.");
+
+				String stringLiteral = String.valueOf(tokens.get(i + 2).getValue());
+
+				String operator = String.valueOf(tokens.get(i + 1).getValue());
+
+				if (operator == null)
+					throw new QueryParserException("Neispravan upit.");
+
+				IComparisonOperator comparisonOperator = getComparitsonOperator(operator);
+
+				expressions.add(new ConditionalExpression(fieldValueGetter, stringLiteral, comparisonOperator));
+
+			} catch (IndexOutOfBoundsException e) {
+				throw new QueryParserException("Neispravan upit.");
+			}
 		}
 		return expressions;
 	}
-	
+
 	/**
 	 * Metoda za dohvar ispravnog operatora usporedbe.
 	 * 
@@ -106,28 +137,41 @@ public class QueryParser {
 	 * @return operator usporedbe tipa {@code IComparisonOperator}
 	 */
 	private IComparisonOperator getComparitsonOperator(String operator) {
-	
-		if(operator.equals("<")) return ComparisonOperators.LESS;
-		if(operator.equals(">")) return ComparisonOperators.GREATER;
-		if(operator.equals(">=")) return ComparisonOperators.GREATER_OR_EQUALS;
-		if(operator.equals("<=")) return ComparisonOperators.LESS_OR_EQUALS;
-		if(operator.equals("=")) return ComparisonOperators.EQUALS;
-		if(operator.equals("!=")) return ComparisonOperators.NOT_EQUALS;
-		else return ComparisonOperators.LIKE;
+
+		if (operator.equals("<"))
+			return ComparisonOperators.LESS;
+		if (operator.equals(">"))
+			return ComparisonOperators.GREATER;
+		if (operator.equals(">="))
+			return ComparisonOperators.GREATER_OR_EQUALS;
+		if (operator.equals("<="))
+			return ComparisonOperators.LESS_OR_EQUALS;
+		if (operator.equals("="))
+			return ComparisonOperators.EQUALS;
+		if (operator.equals("!="))
+			return ComparisonOperators.NOT_EQUALS;
+		if (operator.equals("LIKE"))
+			return ComparisonOperators.LIKE;
+		return null;
 	}
 
 	/**
-	 * Metoda za dohvat odgovarajućeg {@code FieldValueGetter} objekta
-	 * na temelju teksta koji je predan kao argument.
+	 * Metoda za dohvat odgovarajućeg {@code FieldValueGetter} objekta na temelju
+	 * teksta koji je predan kao argument.
 	 * 
-	 * @param atribute tekst na temelju kojeg se dohvaća ispravan {@code FieldValueGetter}
-	 * @return {@code FieldValueGetter} objekt 
+	 * @param atribute tekst na temelju kojeg se dohvaća ispravan
+	 *                 {@code FieldValueGetter}
+	 * @return {@code FieldValueGetter} objekt
 	 */
 	public IFieldValueGetter getFiedlValueGetter(String atribute) {
-		
-		if(atribute.equals("jmbag")) return FieldValueGetters.JMBAG;
-		if(atribute.equals("firstName")) return FieldValueGetters.FIRST_NAME;
-		else return FieldValueGetters.LAST_NAME;
+
+		if (atribute.equals("jmbag"))
+			return FieldValueGetters.JMBAG;
+		if (atribute.equals("firstName"))
+			return FieldValueGetters.FIRST_NAME;
+		if (atribute.equals("lastName"))
+			return FieldValueGetters.LAST_NAME;
+		return null;
 	}
-	
+
 }
