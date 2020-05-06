a3764124d3bec84ae9139b558fc4e85285dc4ad5
kuzzmak <kuzminski.antonio@gmail.com>
2020-05-06 23:22:32 +0200

    updated

--- zadace/hw09-0036503800/src/main/java/hr/fer/zemris/java/fractals/Newton.java
+++ zadace/hw09-0036503800/src/main/java/hr/fer/zemris/java/fractals/Newton.java
@@ -18,11 +18,22 @@ import hr.fer.zemris.math.Complex;
 import hr.fer.zemris.math.ComplexPolynomial;
 import hr.fer.zemris.math.ComplexRootedPolynomial;
 
+/**
+ * Razred zadužen za prikaz fraktala pomoću Newton-Raphson metode pronalaska
+ * aproksimacije korijena.
+ * 
+ * @author Antonio Kuzminski
+ *
+ */
 public class Newton {
 
+	// nultočke polinoma koje je unio korisnik
 	private static Complex[] roots;
+	// razlika između starog i novog kompleksnog broja u jednoj iteraciji algoritma
 	private static double threshold = 1E-3;
+	// tolerancija odmaka trenutnog kompleksnog broja od nekog od korijena
 	private static double rootThreshold = 0.002;
+	// maksimalan broj iteracija u algoritmu
 	private static int maxIterations;
 
 	public Newton(int maxIterations, List<Complex> roots) {
@@ -56,6 +67,13 @@ public class Newton {
 		return new Complex(cre, cim);
 	}
 
+	/**
+	 * Razred koji predstavlja posao jedne dretve, odnosno
+	 * izračun izgleda fraktala jednog dijela kompleksne ravnine.
+	 * 
+	 * @author Antonio Kuzminski
+	 *
+	 */
 	public static class Job implements Callable<Void> {
 
 		double reMin;
@@ -70,6 +88,21 @@ public class Newton {
 		int offset;
 		ComplexRootedPolynomial crp;
 
+		/**
+		 * Inicijalni koknstruktor.
+		 * 
+		 * @param reMin minimalna vrijednost realne komponente kompleksnog broja
+		 * @param reMax maksimalna vrijednost realne komponente kompleksnog broja
+		 * @param imMin minimalna vrijednost imaginarne komponente kompleksnog broja
+		 * @param imMax maksimalna vrijednost imaginarne komponente kompleksnog broja
+		 * @param width širina prozora u kojem se crta
+		 * @param height visina prozora u kojem se crta
+		 * @param yMin donja granica visine prozora od koje počinje crtanje, uključena
+		 * @param yMax gornja granica visine prozora do koje se crta, isključena
+		 * @param data polje u koje se upisuju rezultati
+		 * @param offset pomak od kojeg se crta
+		 * @param crp polinom kojem su nultočke kompleksni brojevi predani od strane korisnika
+		 */
 		public Job(double reMin, double reMax, double imMin, double imMax, int width, int height, int yMin, int yMax,
 				short[] data, int offset, ComplexRootedPolynomial crp) {
 			super();
@@ -100,12 +133,12 @@ public class Newton {
 					double module;
 					int iter = 0;
 
+					// algoritam Newton-Raphson
 					do {
 						Complex znold = zn.copy();
 
 						Complex numerator = polynomial.apply(zn);
 						Complex denominator = polynomial.derive().apply(zn);
-
 						Complex fraction = numerator.divide(denominator);
 						zn = zn.sub(fraction);
 						module = znold.sub(zn).module();
@@ -121,10 +154,21 @@ public class Newton {
 		}
 	}
 
+	/**
+	 * Razred koji je zadužen za vizualizaciju jednog dijela kompleksne ravnine.
+	 * 
+	 * @author Antonio Kuzminski
+	 *
+	 */
 	public static class Producer implements IFractalProducer {
 
 		ExecutorService pool;
 
+		/**
+		 * Inicijalni koknstruktor koji stvara bazen dretvi veličine broja 
+		 * raspoloživih procesora na računalu.
+		 * 
+		 */
 		public Producer() {
 
 			pool = Executors.newFixedThreadPool(
@@ -148,6 +192,7 @@ public class Newton {
 
 			List<Future<Void>> results = new ArrayList<>();
 			
+			// pomak u polju podataka od kojeg pojedina dretva računa fraktal
 			int offset = 0;
 
 			for (int i = 0; i < numberOfJobs; i++) {
@@ -183,7 +228,6 @@ public class Newton {
 				}
 			}
 
-//			pool.shutdown();
 			System.out.println("Racunanje gotovo. Idem obavijestiti promatraca tj. GUI!");
 			observer.acceptResult(data, (short)(polynomial.order() + 1), requestNo);
 		}
@@ -226,4 +270,5 @@ public class Newton {
 		Newton n = new Newton(16, roots);
 		
 	}
+	
 }
