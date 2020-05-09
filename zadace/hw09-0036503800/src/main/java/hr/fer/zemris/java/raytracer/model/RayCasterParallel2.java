fe8dc442933eaf5f0159cc91a3f90ede746d6a32
kuzzmak <kuzminski.antonio@gmail.com>
2020-05-09 14:38:54 +0200

    updated

--- zadace/hw09-0036503800/src/main/java/hr/fer/zemris/java/raytracer/model/RayCasterParallel2.java
+++ zadace/hw09-0036503800/src/main/java/hr/fer/zemris/java/raytracer/model/RayCasterParallel2.java
@@ -15,135 +15,159 @@ public class RayCasterParallel2 {
 	}
 
 	/**
-	 * Funkcija za pronalazak sjecišta trenutne zrake {@code ray} i objekata scene
-	 * {@code scene}. Ako ih postoji više, uzima se bliže.
+	 * Funkcija za pronalazak sjecišta trenutne zrake {@code ray} i objekata
+	 * scene {@code scene}. Ako ih postoji više, uzima se bliže.
 	 * 
 	 * @param scene scene u kojoj se nalaze objekti
-	 * @param ray   zraka kojoj se nalazi potencijalno sjecište
+	 * @param ray zraka kojoj se nalazi potencijalno sjecište
 	 * @return sjecište ako postoji, {@code null} inače
 	 */
 	private static RayIntersection findClosestIntersection(Scene scene, Ray ray) {
-
+		
 		List<GraphicalObject> objects = scene.getObjects();
-
+		
 		double minDistance = Double.MAX_VALUE;
 		RayIntersection closest = null;
-
-		for (GraphicalObject go : objects) {
-
+		
+		for(GraphicalObject go: objects) {
+			
 			RayIntersection intersection = go.findClosestRayIntersection(ray);
-
-			if (intersection != null) {
-				if (intersection.getDistance() < minDistance) {
+			
+			if(intersection != null) {
+				if(intersection.getDistance() < minDistance) {
 					closest = intersection;
 					minDistance = intersection.getDistance();
 				}
 			}
 		}
-
+		
 		return closest;
 	}
-
-	private static short[] diffuseComponent(LightSource ls, Ray fromLStoS, RayIntersection closestObjectIntersection) {
-
-		// polje doprinosa difuzne komponente
-		short[] rgb = new short[3];
-
-		Point3D p1 = ls.getPoint();
-		Point3D p2 = closestObjectIntersection.getNormal();
-
-		Vector3 v1 = new Vector3(p1.x, p1.y, p1.z);
-		Vector3 v2 = new Vector3(p2.x, p2.y, p2.z);
-
-		// kosinus kuta između vektora: od izvor do najbliže točke i normale objekta u
-		// točki
+	
+	/**
+	 * Funkcija za izračun doprinosa difuzne komponente.
+	 * 
+	 * @param ls svjetlosni izvor
+	 * @param closestObjectIntersection točka na objektu koju vidi promatrač
+	 * @param rgb polje kojem se doda doprinos difuzne komponente
+	 * @return doprinos difuzne komponente
+	 */
+	private static void diffuseComponent(LightSource ls, RayIntersection closestObjectIntersection, short[] rgb) {
+		
+		// vektor smjera od točke na objektu prema izvoru svjetla
+		Point3D lightSourceDirection = ls.getPoint().sub(closestObjectIntersection.getPoint());
+		// normala u točki objekta koji se promatra
+		Point3D n = closestObjectIntersection.getNormal();
+		
+		Vector3 v1 = new Vector3(lightSourceDirection.x, lightSourceDirection.y, lightSourceDirection.z);
+		Vector3 v2 = new Vector3(n.x, n.y, n.z);
+		
+		// kosinus kuta između vektora: od izvor do najbliže točke i normale objekta u točki
 		double cosfi = v1.cosAngle(v2);
-
-		rgb[0] = (short) (ls.getR() * closestObjectIntersection.getKdr() * cosfi);
-		rgb[1] = (short) (ls.getG() * closestObjectIntersection.getKdg() * cosfi);
-		rgb[2] = (short) (ls.getB() * closestObjectIntersection.getKdb() * cosfi);
-
-		return rgb;
+		
+		short r = (short) (ls.getR() * closestObjectIntersection.getKdr() * cosfi);
+		short g = (short) (ls.getG() * closestObjectIntersection.getKdg() * cosfi);
+		short b = (short) (ls.getB() * closestObjectIntersection.getKdb() * cosfi);
+		
+		rgb[0] += r;
+		rgb[1] += g;
+		rgb[2] += b;
 	}
-
-	private static short[] reflectiveComponent(LightSource ls, Ray fromLStoS, RayIntersection closestObjectIntersection,
-			Ray ray) {
-
-		short[] rgb = new short[3];
-
+	
+	/**
+	 * Funkcija za izračun doprinosa zrcalne komponente.
+	 * 
+	 * @param ls svjetlosni izvor
+	 * @param closestObjectIntersection točka na objektu koju vidi promatrač
+	 * @param ray zraka od točke promatrača do točke na objektu
+	 * @param rgb doprinos zrcalne komponente
+	 */
+	private static void reflectiveComponent(LightSource ls, RayIntersection closestObjectIntersection, Ray ray, short[] rgb) {
+		
 		Point3D p1 = ls.getPoint();
 		Point3D p2 = closestObjectIntersection.getNormal();
 		Point3D p3 = ray.direction;
-
-		Vector3 l = new Vector3(p1.x, p1.y, p1.z);
+		// vektor od točke na objektu do izvora svjetla
+		Point3D lv = p1.sub(closestObjectIntersection.getPoint()).modifyNormalize();
+		
+		Vector3 l = new Vector3(lv.x, lv.y, lv.z);
+		// normala u točki na objektu
 		Vector3 normal = new Vector3(p2.x, p2.y, p2.z);
-		Vector3 v = new Vector3(p3.x, p3.y, p3.z);
-
+		// vektor od promatrača do točke na objektu
+		Vector3 v = new Vector3(-p3.x, -p3.y, -p3.z);
+		// reflektirana zraka/vektor
 		Vector3 r = normal.scale(2).scale(l.dot(normal)).sub(l);
-
+		
 		double cosa = r.cosAngle(v);
 		double n = closestObjectIntersection.getKrn();
-
-		rgb[0] = (short) (ls.getR() * closestObjectIntersection.getKrr() * Math.pow(cosa, n));
-		rgb[1] = (short) (ls.getG() * closestObjectIntersection.getKrg() * Math.pow(cosa, n));
-		rgb[2] = (short) (ls.getB() * closestObjectIntersection.getKrb() * Math.pow(cosa, n));
-
-		return rgb;
+		
+		rgb[0] += (short) (ls.getR() * closestObjectIntersection.getKrr() * Math.pow(cosa, n));
+		rgb[1] += (short) (ls.getG() * closestObjectIntersection.getKrg() * Math.pow(cosa, n));
+		rgb[2] += (short) (ls.getB() * closestObjectIntersection.getKrb() * Math.pow(cosa, n));
 	}
-
+	
+	/**
+	 * Metoda za provjeru siječe li zraka objekt i bojanje piksela ako ga siječe.
+	 * 
+	 * @param scene scena u kojoj se nalaze objekti
+	 * @param ray zraka za koju se provjerava siječe li koji objekt
+	 * @param rgb crvena, zelena i plava komponenta piksela
+	 */
 	protected static void tracer(Scene scene, Ray ray, short[] rgb) {
-
+		
+		// ambijentna komponenta
 		rgb[0] = 15;
 		rgb[1] = 15;
 		rgb[2] = 15;
-
+		
 		RayIntersection closestObjectIntersection = findClosestIntersection(scene, ray);
-
-		if (closestObjectIntersection == null)
-			return;
-
+		
+		if(closestObjectIntersection == null) return;
+		
 		List<LightSource> lightSources = scene.getLights();
-
-		for (LightSource ls : lightSources) {
-
-			Ray fromLStoS = Ray.fromPoints(ls.getPoint(), closestObjectIntersection.getPoint());
-
-			RayIntersection closestLightSourceIntersection = findClosestIntersection(scene, fromLStoS);
-
-			if (closestLightSourceIntersection != null) {
-
+		
+		for(LightSource ls: lightSources) {
+			
+			Ray fromLStoObject = Ray.fromPoints(ls.getPoint(), closestObjectIntersection.getPoint());
+			
+			RayIntersection closestLightSourceIntersection = findClosestIntersection(scene, fromLStoObject);
+			
+			if(closestLightSourceIntersection != null) {
+				
 				// udaljenost od izvora svjetla do najbliže točke
 				double d1 = closestLightSourceIntersection.getDistance();
-
+				
 				// točka najbliža izvoru svjetla
 				Point3D lsPoint = ls.getPoint();
 				// točka najbliža gledatelju
 				Point3D objectPoint = closestLightSourceIntersection.getPoint();
-
+				
 				// udaljenost od izvora svjetla do najbliže točke na objektu koja
 				// je izračunata kao najbliža na nekom objektu
-				double d2 = Math.sqrt(Math.pow(lsPoint.x - objectPoint.x, 2) + Math.pow(lsPoint.y - objectPoint.y, 2)
-						+ Math.pow(lsPoint.z - objectPoint.z, 2));
-
+				double d2 = Math.sqrt(Math.pow(lsPoint.x - objectPoint.x, 2) + 
+						Math.pow(lsPoint.y - objectPoint.y, 2) +
+						Math.pow(lsPoint.z - objectPoint.z, 2));
+				
 				// objekt zaklonjen
-				if (d2 - d1 > 1E-6) {
+				if(d1 - d2 < 0) {
 					continue;
-				} else {
-
-					short[] diff = diffuseComponent(ls, fromLStoS, closestObjectIntersection);
-					rgb[0] += diff[0];
-					rgb[1] += diff[1];
-					rgb[2] += diff[2];
-
-					short[] ref = reflectiveComponent(ls, fromLStoS, closestObjectIntersection, ray);
-					rgb[0] += ref[0];
-					rgb[1] += ref[1];
-					rgb[2] += ref[2];
+				}else {
+					diffuseComponent(ls, closestObjectIntersection, rgb);
+					reflectiveComponent(ls, closestObjectIntersection, ray, rgb);
 				}
 			}
 		}
 	}
 
+	/**
+	 * Razred koji predstavlja posao izračuna piksela. Posao izračuna
+	 * se raspolavlja tako dugo do kada broj redaka piksela koji
+	 * svaki objekt ovog razreda mora napraviti ne pdane ispod broja 
+	 * {@code threshold}.
+	 * 
+	 * @author Antonio Kuzminski
+	 *
+	 */
 	private static class Job extends RecursiveAction {
 
 		private static final long serialVersionUID = 1L;
@@ -194,6 +218,11 @@ public class RayCasterParallel2 {
 							vertical, red, green, blue));
 		}
 
+		/**
+		 * Funkcija za direktan izračun zadatka ako je broj redaka
+		 * piksela ispod broja {@code threshold}.
+		 * 
+		 */
 		protected void computeDirect() {
 			
 			int offset = 0;
@@ -222,6 +251,11 @@ public class RayCasterParallel2 {
 
 	}
 	
+	/**
+	 * Metoda za stvaranje objekta animacije.
+	 * 
+	 * @return objekt tipa {@code IRayTracerAnimator}
+	 */
 	private static IRayTracerAnimator getIRayTracerAnimator() {
 		return new IRayTracerAnimator() {
 			
@@ -260,6 +294,11 @@ public class RayCasterParallel2 {
 		
 	}
 
+	/**
+	 * Metoda za stvaranje ray tracer objekta.
+	 * 
+	 * @return objekt tipa {@code IRayTracerProducer}
+	 */
 	private static IRayTracerProducer getIRayTracerProducer() {
 		return new IRayTracerProducer() {
 
@@ -272,18 +311,6 @@ public class RayCasterParallel2 {
 				short[] red = new short[width * height];
 				short[] green = new short[width * height];
 				short[] blue = new short[width * height];
-
-				Point3D g_o = view.sub(eye);
-				Point3D og = g_o.scalarMultiply(1 / g_o.norm());
-				Point3D viewNormalized = viewUp.normalize();
-
-				Point3D yAxis = viewNormalized.sub(og.scalarMultiply(og.scalarProduct(viewNormalized)));
-				yAxis.modifyNormalize();
-
-				Point3D xAxis = og.vectorProduct(yAxis);
-				xAxis.modifyNormalize();
-
-				Point3D zAxis = og.copy();
 
 				Point3D screenCorner = new Point3D(view.x, view.y - horizontal / 2, view.z + vertical / 2);
 
