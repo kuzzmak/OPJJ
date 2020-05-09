6b3ee8ee76216325f7c7da82510556e81e3abf5b
kuzzmak <kuzminski.antonio@gmail.com>
2020-05-09 12:17:08 +0200

    bug fix

--- zadace/hw09-0036503800/src/main/java/hr/fer/zemris/java/raytracer/model/RayCaster.java
+++ zadace/hw09-0036503800/src/main/java/hr/fer/zemris/java/raytracer/model/RayCaster.java
@@ -9,7 +9,6 @@ import hr.fer.zemris.math.Vector3;
 
 public class RayCaster {
 	
-	
 	public static void main(String[] args) {
 		RayTracerViewer.show(getIRayTracerProducer(), 
 				new Point3D(10, 0, 0),
@@ -49,56 +48,77 @@ public class RayCaster {
 		return closest;
 	}
 	
-	private static short[] diffuseComponent(LightSource ls, Ray fromLStoS, RayIntersection closestObjectIntersection) {
+	/**
+	 * Funkcija za izračun doprinosa difuzne komponente.
+	 * 
+	 * @param ls svjetlosni izvor
+	 * @param closestObjectIntersection točka na objektu koju vidi promatrač
+	 * @param rgb polje kojem se doda doprinos difuzne komponente
+	 * @return doprinos difuzne komponente
+	 */
+	private static void diffuseComponent(LightSource ls, RayIntersection closestObjectIntersection, short[] rgb) {
 		
-		// polje doprinosa difuzne komponente
-		short[] rgb = new short[3];
-		
+		// vektor smjera od točke na objektu prema izvoru svjetla
 		Point3D lightSourceDirection = ls.getPoint().sub(closestObjectIntersection.getPoint());
+		// normala u točki objekta koji se promatra
 		Point3D n = closestObjectIntersection.getNormal();
 		
 		Vector3 v1 = new Vector3(lightSourceDirection.x, lightSourceDirection.y, lightSourceDirection.z);
 		Vector3 v2 = new Vector3(n.x, n.y, n.z);
 		
-		
 		// kosinus kuta između vektora: od izvor do najbliže točke i normale objekta u točki
 		double cosfi = v1.cosAngle(v2);
 		
-//		System.out.println(closestObjectIntersection.getKdr());
+		short r = (short) (ls.getR() * closestObjectIntersection.getKdr() * cosfi);
+		short g = (short) (ls.getG() * closestObjectIntersection.getKdg() * cosfi);
+		short b = (short) (ls.getB() * closestObjectIntersection.getKdb() * cosfi);
 		
-		rgb[0] = (short) (ls.getR() * closestObjectIntersection.getKdr() * cosfi);
-		rgb[1] = (short) (ls.getG() * closestObjectIntersection.getKdg() * cosfi);
-		rgb[2] = (short) (ls.getB() * closestObjectIntersection.getKdb() * cosfi);
-		
-		return rgb;
+		rgb[0] += r > 0 ? r : 0;
+		rgb[1] += g > 0 ? g : 0;
+		rgb[2] += b > 0 ? b : 0;
 	}
 	
-	private static short[] reflectiveComponent(LightSource ls, Ray fromLStoS, RayIntersection closestObjectIntersection, Ray ray) {
-		
-		short[] rgb = new short[3];
+	/**
+	 * Funkcija za izračun doprinosa zrcalne komponente.
+	 * 
+	 * @param ls svjetlosni izvor
+	 * @param closestObjectIntersection točka na objektu koju vidi promatrač
+	 * @param ray zraka od točke promatrača do točke na objektu
+	 * @param rgb doprinos zrcalne komponente
+	 */
+	private static void reflectiveComponent(LightSource ls, RayIntersection closestObjectIntersection, Ray ray, short[] rgb) {
 		
 		Point3D p1 = ls.getPoint();
 		Point3D p2 = closestObjectIntersection.getNormal();
 		Point3D p3 = ray.direction;
 		
+		// vektor od točke na objektu do izvora svjetla
 		Vector3 l = new Vector3(p1.x, p1.y, p1.z);
+		// normala u točki na objektu
 		Vector3 normal = new Vector3(p2.x, p2.y, p2.z);
+		// vektor od promatrača do točke na objektu
 		Vector3 v = new Vector3(p3.x, p3.y, p3.z);
-		
+		// reflektirana zraka/vektor
 		Vector3 r = normal.scale(2).scale(l.dot(normal)).sub(l);
 		
 		double cosa = r.cosAngle(v);
 		double n = closestObjectIntersection.getKrn();
 		
-		rgb[0] = (short) (ls.getR() * closestObjectIntersection.getKrr() * Math.pow(cosa, n));
-		rgb[1] = (short) (ls.getG() * closestObjectIntersection.getKrg() * Math.pow(cosa, n));
-		rgb[2] = (short) (ls.getB() * closestObjectIntersection.getKrb() * Math.pow(cosa, n));
-		
-		return rgb;
+		rgb[0] += (short) (ls.getR() * closestObjectIntersection.getKrr() * Math.pow(cosa, n));
+		rgb[1] += (short) (ls.getG() * closestObjectIntersection.getKrg() * Math.pow(cosa, n));
+		rgb[2] += (short) (ls.getB() * closestObjectIntersection.getKrb() * Math.pow(cosa, n));
 	}
 	
+	/**
+	 * Metoda za provjeru siječe li zraka objekt i bojanje piksela ako ga siječe.
+	 * 
+	 * @param scene scena u kojoj se nalaze objekti
+	 * @param ray zraka za koju se provjerava siječe li koji objekt
+	 * @param rgb crvena, zelena i plava komponenta piksela
+	 */
 	protected static void tracer(Scene scene, Ray ray, short[] rgb) {
 		
+		// ambijentna komponenta
 		rgb[0] = 15;
 		rgb[1] = 15;
 		rgb[2] = 15;
@@ -111,9 +131,9 @@ public class RayCaster {
 		
 		for(LightSource ls: lightSources) {
 			
-			Ray fromLStoS = Ray.fromPoints(ls.getPoint(), closestObjectIntersection.getPoint());
+			Ray fromLStoObject = Ray.fromPoints(ls.getPoint(), closestObjectIntersection.getPoint());
 			
-			RayIntersection closestLightSourceIntersection = findClosestIntersection(scene, fromLStoS);
+			RayIntersection closestLightSourceIntersection = findClosestIntersection(scene, fromLStoObject);
 			
 			if(closestLightSourceIntersection != null) {
 				
@@ -132,25 +152,21 @@ public class RayCaster {
 						Math.pow(lsPoint.z - objectPoint.z, 2));
 				
 				// objekt zaklonjen
-				if(d2 - d1 > 1E-6) {
+				if(d1 - d2 < 0) {
 					continue;
 				}else {
-					
-					short[] diff = diffuseComponent(ls, fromLStoS, closestObjectIntersection);
-					rgb[0] += diff[0];
-					rgb[1] += diff[1];
-					rgb[2] += diff[2];
-					
-					short[] ref = reflectiveComponent(ls, fromLStoS, closestObjectIntersection, ray);
-					rgb[0] += ref[0];
-					rgb[1] += ref[1];
-					rgb[2] += ref[2];
+					diffuseComponent(ls, closestObjectIntersection, rgb);
+					reflectiveComponent(ls, closestObjectIntersection, ray, rgb);
 				}
 			}
 		}
 	}
 	
-	
+	/**
+	 * Metoda za stvaranje objekta za ray tracing.
+	 * 
+	 * @return objekt tipa {@code IRayTracerProducer}
+	 */
 	private static IRayTracerProducer getIRayTracerProducer() {
 		return new IRayTracerProducer() {
 			
@@ -168,58 +184,34 @@ public class RayCaster {
 				Point3D og = g_o.scalarMultiply(1 / g_o.norm());
 				Point3D viewNormalized = viewUp.normalize();
 
-				Point3D yAxis = viewNormalized.sub(og.scalarMultiply(og.scalarProduct(viewNormalized)));
-		        yAxis.modifyNormalize();
-
-		        Point3D xAxis = og.vectorProduct(yAxis);
-		        xAxis.modifyNormalize();
-		        
-		        Point3D zAxis = og.copy();
+//				Point3D yAxis = viewNormalized.sub(og.scalarMultiply(og.scalarProduct(viewNormalized)));
+//		        yAxis.modifyNormalize();
+//
+//		        Point3D xAxis = og.vectorProduct(yAxis);
+//		        xAxis.modifyNormalize();
+//		        
+//		        Point3D zAxis = og.copy();
 		        
 		        Point3D screenCorner = new Point3D(
 		        		view.x, 
 		        		view.y - horizontal / 2, 
 		        		view.z + vertical / 2);
 		        
-//		        Point3D screenCorner = new Point3D(
-//		        		view.x - horizontal / 2, 
-//		        		view.y + vertical / 2, 
-//		        		view.z);
-		        
-//				System.out.println("z: " + zAxis.x + ", " + zAxis.y + ", " + zAxis.z);
-//				System.out.println("screen corner: " + screenCorner.x + ", " + screenCorner.y + ", " + screenCorner.z);
-				
-				
 				Scene scene = RayTracerViewer.createPredefinedScene();
 				
 				short[] rgb = new short[3];
 				int offset = 0;
 				
-//				int x = 250;
-//				int y = 166;
-				
 				for(int y = 0; y < height; y++) {
 					for(int x = 0; x < width; x++) {
-						
-						
 						
 						Point3D screenPoint = new Point3D(screenCorner.x,
 				        		screenCorner.y  + (double)x / (width - 1) * horizontal,
 				        		screenCorner.z - (double)y / (height - 1) * vertical);
 						
-//						Point3D screenPoint = new Point3D(screenCorner.x + (double)x / (width - 1) * horizontal,
-//								screenCorner.y - (double)y / (height - 1) * vertical,
-//								screenCorner.z);
-						
 						Ray ray = Ray.fromPoints(eye, screenPoint);
 						
-//						System.out.println("screen point: " + screenPoint.x + ", " + screenPoint.y + ", " + screenPoint.z);
-//						System.out.println("ray start: " + ray.start.x + ", " + ray.start.y + ", " + ray.start.z);
-//						System.out.println("direction: " + ray.direction.x + ", " + ray.direction.y + ", " + ray.direction.z);
-						
 						tracer(scene, ray, rgb);
-						
-//						System.out.println(Arrays.toString(rgb));
 						
 						red[offset] = rgb[0] > 255 ? 255 : rgb[0];
 						green[offset] = rgb[1] > 255 ? 255 : rgb[1];
