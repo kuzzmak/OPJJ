9ff91f4009781b488aa644d60798c23c0501df2a
kuzzmak <kuzminski.antonio@gmail.com>
2020-05-16 18:53:15 +0200

    added javadoc

--- zadace/hw10-0036503800/src/main/java/hr/fer/zemris/java/gui/layouts/CalcLayoutException.java
+++ zadace/hw10-0036503800/src/main/java/hr/fer/zemris/java/gui/layouts/CalcLayoutException.java
@@ -1,5 +1,12 @@
 package hr.fer.zemris.java.gui.layouts;
 
+/**
+ * Razred iznimke koji se poziva ukoliko je došlo do kakve greške prilikom
+ * korištenja kalkulatora.
+ * 
+ * @author Antonio Kuzminski
+ *
+ */
 public class CalcLayoutException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
