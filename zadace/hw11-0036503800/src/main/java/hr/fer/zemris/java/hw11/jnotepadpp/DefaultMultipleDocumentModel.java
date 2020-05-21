d1d8c94ca0138ce3c9a8409785940707bbeef968
kuzzmak <kuzminski.antonio@gmail.com>
2020-05-21 21:58:44 +0200

    updated loading and document saving

--- zadace/hw11-0036503800/src/main/java/hr/fer/zemris/java/hw11/jnotepadpp/DefaultMultipleDocumentModel.java
+++ zadace/hw11-0036503800/src/main/java/hr/fer/zemris/java/hw11/jnotepadpp/DefaultMultipleDocumentModel.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
@@ -23,33 +24,25 @@ import javax.swing.event.ChangeListener;
 public class DefaultMultipleDocumentModel extends JTabbedPane implements MultipleDocumentModel {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	List<SingleDocumentModel> documents;
-	
+
 	List<MultipleDocumentListener> listeners;
-	
+
 	private ImageIcon unmodified;
 	private ImageIcon modified;
-	
-	
-	
+
 	/**
 	 * Konstruktor.
 	 * 
 	 */
 	public DefaultMultipleDocumentModel() {
-		
+
 		documents = new ArrayList<>();
 		listeners = new ArrayList<>();
-		
-		SingleDocumentModel newDocument = createNewDocument();
-		JScrollPane scp = new JScrollPane(newDocument.getTextComponent());
+
 		unmodified = createImageIcon("icons/unmodified.png", 20);
 		modified = createImageIcon("icons/modified.png", 20);
-		
-		newTab(null, null, scp);
-		
-		
 	}
 
 	@Override
@@ -59,7 +52,7 @@ public class DefaultMultipleDocumentModel extends JTabbedPane implements Multipl
 
 	@Override
 	public SingleDocumentModel createNewDocument() {
-		
+
 		SingleDocumentModel newDocument = new DefaultSingleDocumentModel(null, "");
 		documents.add(newDocument);
 		return newDocument;
@@ -72,49 +65,78 @@ public class DefaultMultipleDocumentModel extends JTabbedPane implements Multipl
 
 	@Override
 	public SingleDocumentModel loadDocument(Path path) {
-		
-		if (!Files.isReadable(path)) {
-			JOptionPane.showMessageDialog(this,
-					"Datoteka " + path + " ne postoji!", "Pogreška",
-					JOptionPane.ERROR_MESSAGE);
-			return null;
+
+		int indexOfAlredyOpened = -1;
+
+		// provjera je li već otvorena datoteka koja je odabrana
+		for (int i = 0; i < documents.size(); i++) {
+
+			Path documentPath = documents.get(i).getFilePath();
+			if (documentPath == null)
+				continue;
+
+			if (documentPath.equals(path)) {
+				indexOfAlredyOpened = i;
+			}
 		}
-		
-		byte[] bytes;
-		try {
-			bytes = Files.readAllBytes(path);
-		} catch (Exception ex) {
-			JOptionPane.showMessageDialog(this,
-					"Pogreška prilikom čitanja datoteke " + path + ".", "Pogreška",
-					JOptionPane.ERROR_MESSAGE);
-			return null;
+
+		if (indexOfAlredyOpened == -1) {
+
+			byte[] bytes;
+			try {
+				bytes = Files.readAllBytes(path);
+			} catch (Exception ex) {
+				JOptionPane.showMessageDialog(this, "Pogreška prilikom čitanja datoteke " + path + ".", "Pogreška",
+						JOptionPane.ERROR_MESSAGE);
+				return null;
+			}
+
+			try {
+				bytes = Files.readAllBytes(path);
+			} catch (Exception ex) {
+				JOptionPane.showMessageDialog(this, "Pogreška prilikom čitanja datoteke " + path + ".", "Pogreška",
+						JOptionPane.ERROR_MESSAGE);
+				return null;
+			}
+
+			String text = new String(bytes, StandardCharsets.UTF_8);
+			SingleDocumentModel newDocument = new DefaultSingleDocumentModel(path, text);
+			documents.add(newDocument);
+
+			newTab(path.getFileName().toString(), null, new JScrollPane(newDocument.getTextComponent()));
+
+			// odabir taba novo učitane datoteke
+			setSelectedIndex(documents.size() - 1);
+			return newDocument;
+
+		} else {
+
+			// odabir kartice koja je već otvorena
+			setSelectedIndex(indexOfAlredyOpened);
+			return getDocument(indexOfAlredyOpened);
 		}
-		
-		try {
-			bytes = Files.readAllBytes(path);
-		} catch (Exception ex) {
-			JOptionPane.showMessageDialog(this,
-					"Pogreška prilikom čitanja datoteke " + path + ".", "Pogreška",
-					JOptionPane.ERROR_MESSAGE);
-			return null;
-		}
-		
-		String text = new String(bytes, StandardCharsets.UTF_8);
-		SingleDocumentModel newDocument = new DefaultSingleDocumentModel(path, text);
-		documents.add(newDocument);
-		
-		newTab(path.getFileName().toString(), null, new JScrollPane(newDocument.getTextComponent()));
-		
-		// odabir taba novo učitane datoteke
-		setSelectedIndex(documents.size() - 1);
-		
-		return newDocument;
 	}
 
 	@Override
 	public void saveDocument(SingleDocumentModel model, Path newPath) {
-		// TODO Auto-generated method stub
 
+		byte[] podatci = model.getTextComponent().getText().getBytes(StandardCharsets.UTF_8);
+		
+		try {
+			Files.write(newPath, podatci);
+		} catch (IOException e1) {
+			JOptionPane.showMessageDialog(this,
+					"Pogreška prilikom zapisivanja datoteke " + newPath.toFile().getAbsolutePath()
+							+ ".\nPažnja: nije jasno u kojem je stanju datoteka na disku!",
+					"Pogreška", JOptionPane.ERROR_MESSAGE);
+			return;
+		}
+		
+		JOptionPane.showMessageDialog(this, "Datoteka je snimljena.", "Informacija",
+				JOptionPane.INFORMATION_MESSAGE);
+
+		model.setFilePath(newPath);
+		setTitleAt(getSelectedIndex(), model.getFilePath().getFileName().toString());
 	}
 
 	@Override
@@ -142,62 +164,63 @@ public class DefaultMultipleDocumentModel extends JTabbedPane implements Multipl
 
 	@Override
 	public SingleDocumentModel getDocument(int index) {
-		
+
 		int size = getNumberOfDocuments();
-		
-		if(index < 0 || index >= size)
+
+		if (index < 0 || index >= size)
 			throw new IndexOutOfBoundsException("Index izvan granica.");
-		
+
 		return documents.get(index);
 	}
-	
+
 	/**
-	 * Funkcija za učitavanje i stvaranje objekta {@code ImageIcon} iz 
-	 * predane staze do željene sličice {@code path} uz željeno 
-	 * skaliranje {@code scalePercent}.
+	 * Funkcija za učitavanje i stvaranje objekta {@code ImageIcon} iz predane staze
+	 * do željene sličice {@code path} uz željeno skaliranje {@code scalePercent}.
 	 * 
-	 * @param path staza do slike koja se učitava
+	 * @param path         staza do slike koja se učitava
 	 * @param scalePercent postotak na koj se skalira izvorna slika
 	 * @return objekt tipa {@code ImageIcon}
 	 */
 	private ImageIcon createImageIcon(String path, int scalePercent) {
-		
+
 		try (InputStream is = this.getClass().getResourceAsStream(path)) {
-			
+
 			ImageIcon im = new ImageIcon(is.readAllBytes());
 			return new ImageIcon(im.getImage().getScaledInstance(scalePercent, scalePercent, Image.SCALE_DEFAULT));
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		return null;
 	}
-	
+
 	/**
 	 * Funkcija za dodavanje nove kratice u {@code JTabbedPanel}
 	 * 
-	 * @param name ime na kratici
-	 * @param icon ikona prikazana na kratici
+	 * @param name      ime na kratici
+	 * @param icon      ikona prikazana na kratici
 	 * @param component komponenta koja se dodaje
 	 */
 	public void newTab(String name, Icon icon, Component component) {
-		
+
 		Icon tabIcon = icon;
 		String tabName = name;
-		
-		if(icon == null) tabIcon = unmodified;
-		if(name == null) tabName = "(unnamed)";
-		
+
+		if (icon == null)
+			tabIcon = unmodified;
+		if (name == null)
+			tabName = "(unnamed)";
+
 		addTab(tabName, tabIcon, component);
-		
+
 		setSelectedIndex(getTabCount() - 1);
 	}
-	
+
 //	protected void addTab(String name, SingleDocumentModel newDocument) {
 //		
 //		JScrollPane scp = new JScrollPane(newDocument.getTextComponent());
 //		addTab(name, scp);
 //	}
-	
+
 }
