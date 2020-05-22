0092cb808340955df26745ab3cb1a6d085def606
kuzzmak <kuzminski.antonio@gmail.com>
2020-05-22 11:59:40 +0200

    refactoring

--- zadace/hw11-0036503800/src/main/java/hr/fer/zemris/java/hw11/jnotepadpp/DefaultMultipleDocumentModel.java
+++ zadace/hw11-0036503800/src/main/java/hr/fer/zemris/java/hw11/jnotepadpp/DefaultMultipleDocumentModel.java
@@ -29,9 +29,6 @@ public class DefaultMultipleDocumentModel extends JTabbedPane implements Multipl
 
 	List<MultipleDocumentListener> listeners;
 
-	private ImageIcon unmodified;
-	private ImageIcon modified;
-
 	/**
 	 * Konstruktor.
 	 * 
@@ -40,9 +37,6 @@ public class DefaultMultipleDocumentModel extends JTabbedPane implements Multipl
 
 		documents = new ArrayList<>();
 		listeners = new ArrayList<>();
-
-		unmodified = createImageIcon("icons/unmodified.png", 20);
-		modified = createImageIcon("icons/modified.png", 20);
 	}
 
 	@Override
@@ -66,55 +60,28 @@ public class DefaultMultipleDocumentModel extends JTabbedPane implements Multipl
 	@Override
 	public SingleDocumentModel loadDocument(Path path) {
 
-		int indexOfAlredyOpened = -1;
-
-		// provjera je li već otvorena datoteka koja je odabrana
-		for (int i = 0; i < documents.size(); i++) {
-
-			Path documentPath = documents.get(i).getFilePath();
-			if (documentPath == null)
-				continue;
-
-			if (documentPath.equals(path)) {
-				indexOfAlredyOpened = i;
-			}
+		byte[] bytes;
+		try {
+			bytes = Files.readAllBytes(path);
+		} catch (Exception ex) {
+			JOptionPane.showMessageDialog(this, "Pogreška prilikom čitanja datoteke " + path + ".", "Pogreška",
+					JOptionPane.ERROR_MESSAGE);
+			return null;
 		}
 
-		if (indexOfAlredyOpened == -1) {
-
-			byte[] bytes;
-			try {
-				bytes = Files.readAllBytes(path);
-			} catch (Exception ex) {
-				JOptionPane.showMessageDialog(this, "Pogreška prilikom čitanja datoteke " + path + ".", "Pogreška",
-						JOptionPane.ERROR_MESSAGE);
-				return null;
-			}
-
-			try {
-				bytes = Files.readAllBytes(path);
-			} catch (Exception ex) {
-				JOptionPane.showMessageDialog(this, "Pogreška prilikom čitanja datoteke " + path + ".", "Pogreška",
-						JOptionPane.ERROR_MESSAGE);
-				return null;
-			}
-
-			String text = new String(bytes, StandardCharsets.UTF_8);
-			SingleDocumentModel newDocument = new DefaultSingleDocumentModel(path, text);
-			documents.add(newDocument);
-
-			newTab(path.getFileName().toString(), null, new JScrollPane(newDocument.getTextComponent()));
-
-			// odabir taba novo učitane datoteke
-			setSelectedIndex(documents.size() - 1);
-			return newDocument;
-
-		} else {
-
-			// odabir kartice koja je već otvorena
-			setSelectedIndex(indexOfAlredyOpened);
-			return getDocument(indexOfAlredyOpened);
+		try {
+			bytes = Files.readAllBytes(path);
+		} catch (Exception ex) {
+			JOptionPane.showMessageDialog(this, "Pogreška prilikom čitanja datoteke " + path + ".", "Pogreška",
+					JOptionPane.ERROR_MESSAGE);
+			return null;
 		}
+
+		String text = new String(bytes, StandardCharsets.UTF_8);
+		SingleDocumentModel newDocument = new DefaultSingleDocumentModel(path, text);
+		documents.add(newDocument);
+
+		return newDocument;
 	}
 
 	@Override
@@ -138,23 +105,23 @@ public class DefaultMultipleDocumentModel extends JTabbedPane implements Multipl
 		JOptionPane.showMessageDialog(this, "Datoteka je snimljena.", "Informacija", JOptionPane.INFORMATION_MESSAGE);
 
 		model.setFilePath(newPath);
-		setTitleAt(getSelectedIndex(), model.getFilePath().getFileName().toString());
 	}
 
 	@Override
 	public void closeDocument(SingleDocumentModel model) {
 
 		if (model != null) {
-			
+
 			Iterator<SingleDocumentModel> documentIterator = iterator();
-			
-			while(documentIterator.hasNext()) {
-				
+
+			while (documentIterator.hasNext()) {
+
 				SingleDocumentModel doc = documentIterator.next();
-				if(doc.equals(model)) {
+				if (doc.equals(model)) {
 					documentIterator.remove();
 					int i = getSelectedIndex();
-				    if (i != -1) this.removeTabAt(i);
+					if (i != -1)
+						this.removeTabAt(i);
 				}
 			}
 		}
@@ -181,54 +148,11 @@ public class DefaultMultipleDocumentModel extends JTabbedPane implements Multipl
 	public SingleDocumentModel getDocument(int index) {
 
 		int size = getNumberOfDocuments();
-		
-		if (index < 0 || index >= size) return null;
-			
+
+		if (index < 0 || index >= size)
+			return null;
+
 		return documents.get(index);
-	}
-
-	/**
-	 * Funkcija za učitavanje i stvaranje objekta {@code ImageIcon} iz predane staze
-	 * do željene sličice {@code path} uz željeno skaliranje {@code scalePercent}.
-	 * 
-	 * @param path         staza do slike koja se učitava
-	 * @param scalePercent postotak na koj se skalira izvorna slika
-	 * @return objekt tipa {@code ImageIcon}
-	 */
-	private ImageIcon createImageIcon(String path, int scalePercent) {
-
-		try (InputStream is = this.getClass().getResourceAsStream(path)) {
-
-			ImageIcon im = new ImageIcon(is.readAllBytes());
-			return new ImageIcon(im.getImage().getScaledInstance(scalePercent, scalePercent, Image.SCALE_DEFAULT));
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		return null;
-	}
-
-	/**
-	 * Funkcija za dodavanje nove kratice u {@code JTabbedPanel}
-	 * 
-	 * @param name      ime na kratici
-	 * @param icon      ikona prikazana na kratici
-	 * @param component komponenta koja se dodaje
-	 */
-	public void newTab(String name, Icon icon, Component component) {
-
-		Icon tabIcon = icon;
-		String tabName = name;
-
-		if (icon == null)
-			tabIcon = unmodified;
-		if (name == null)
-			tabName = "(unnamed)";
-
-		addTab(tabName, tabIcon, component);
-
-		setSelectedIndex(getTabCount() - 1);
 	}
 
 }
