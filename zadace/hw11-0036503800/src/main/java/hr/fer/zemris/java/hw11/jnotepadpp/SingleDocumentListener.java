dcde0cd3347b86eed1985baf4db833d1010f1a89
kuzzmak <kuzminski.antonio@gmail.com>
2020-05-25 11:53:48 +0200

    updated javadoc

--- zadace/hw11-0036503800/src/main/java/hr/fer/zemris/java/hw11/jnotepadpp/SingleDocumentListener.java
+++ zadace/hw11-0036503800/src/main/java/hr/fer/zemris/java/hw11/jnotepadpp/SingleDocumentListener.java
@@ -1,6 +1,24 @@
 package hr.fer.zemris.java.hw11.jnotepadpp;
 
+/**
+ * Sučelje koje modelira slušaća za dokument tipa {@code SingleDocumentModel}.
+ * 
+ * @author Antonio Kuzminski
+ *
+ */
 public interface SingleDocumentListener {
+	
+	/**
+	 * Metoda koja se poziva uslijed neke vrste modifikacije dokumenta.
+	 * 
+	 * @param model dokument na kojem je napravljena modifikacija
+	 */
 	void documentModifyStatusUpdated(SingleDocumentModel model);
+	
+	/**
+	 * Metoda koja se poziva uslijed promjene staze dokumenta.
+	 * 
+	 * @param model dokument čija je staza promijenjena
+	 */
 	void documentFilePathUpdated(SingleDocumentModel model);
 }
