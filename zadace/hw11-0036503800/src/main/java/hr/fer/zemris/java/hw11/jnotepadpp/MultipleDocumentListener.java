65e8f541fbece88c7c465a08596de0b48dd6f81e
kuzzmak <kuzminski.antonio@gmail.com>
2020-05-25 12:03:43 +0200

    updated javadoc

--- zadace/hw11-0036503800/src/main/java/hr/fer/zemris/java/hw11/jnotepadpp/MultipleDocumentListener.java
+++ zadace/hw11-0036503800/src/main/java/hr/fer/zemris/java/hw11/jnotepadpp/MultipleDocumentListener.java
@@ -1,8 +1,32 @@
 package hr.fer.zemris.java.hw11.jnotepadpp;
 
+/**
+ * Sučelje koje modelira slušaća razreda {@code MultipleDocumentModel}.
+ * 
+ * @author Antonio Kuzminski
+ *
+ */
 public interface MultipleDocumentListener {
 
+	/**
+	 * Metoda koja se poziva prilikom nastalih promjena na dokumentu.
+	 * 
+	 * @param previousModel prijašnje stanje dokumenta
+	 * @param currentModel trenutno stanje dokumenta
+	 */
 	void currentDocumentChanged(SingleDocumentModel previousModel, SingleDocumentModel currentModel);
+	
+	/**
+	 * Metoda koja se poziva prilikom otvaranja ili stvaranja novog dokumenta.
+	 * 
+	 * @param model dokument koji je dodan 
+	 */
 	void documentAdded(SingleDocumentModel model);
+	
+	/**
+	 * Metoda za obavještavanje pretplaćenih slušaća da je uklonjen dokument.
+	 * 
+	 * @param model dokument koji je uklonjen
+	 */
 	void documentRemoved(SingleDocumentModel model);
 }
