263e89833248245afa8801c2def52b53311e073a
kuzzmak <kuzminski.antonio@gmail.com>
2020-05-29 17:34:36 +0200

    workers added

---
+++ zadace/hw12-0036503800/src/main/java/hr/fer/zemris/java/webserver/workers/CircleWorker.java
@@ -0,0 +1,36 @@
+package hr.fer.zemris.java.webserver.workers;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.nio.charset.StandardCharsets;
+
+import javax.imageio.ImageIO;
+
+import hr.fer.zemris.java.webserver.IWebWorker;
+import hr.fer.zemris.java.webserver.RequestContext;
+
+public class CircleWorker implements IWebWorker {
+
+	@Override
+	public void processRequest(RequestContext context) throws Exception {
+		
+		BufferedImage bim = new BufferedImage(
+				200, 200, BufferedImage.TYPE_3BYTE_BGR);
+		
+		Graphics2D g2d = bim.createGraphics();
+		g2d.setColor(Color.WHITE);
+		g2d.fillRect(0, 0, bim.getWidth(), bim.getHeight());
+		g2d.setColor(Color.RED);
+		g2d.fillOval(0, 0, bim.getWidth(), bim.getHeight());
+		g2d.dispose();
+		
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		ImageIO.write(bim, "png", bos);
+		byte[] podatci = bos.toByteArray();
+		
+		context.write(podatci);
+	}
+
+}
