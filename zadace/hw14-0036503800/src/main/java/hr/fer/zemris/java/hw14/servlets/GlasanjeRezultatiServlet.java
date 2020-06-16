edde7914831b03422701021154ed19c0f995b6b0
kuzzmak <kuzminski.antonio@gmail.com>
2020-06-16 22:24:09 +0200

    voting results servlet

---
+++ zadace/hw14-0036503800/src/main/java/hr/fer/zemris/java/hw14/servlets/GlasanjeRezultatiServlet.java
@@ -0,0 +1,40 @@
+package hr.fer.zemris.java.hw14.servlets;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import hr.fer.zemris.java.hw14.DAOProvider;
+import hr.fer.zemris.java.hw14.PollEntry;
+
+/**
+ * Servlet za prikaz stranice rezultata glasanja za najbolji bend.
+ * 
+ * @author Antonio Kuzminski
+ *
+ */
+@WebServlet(name="glasanje-rezultati", urlPatterns = {"/servleti/glasanje-rezultati"})
+public class GlasanjeRezultatiServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		// unosi pojedine ankete
+		List<PollEntry> entries = DAOProvider.getDao().getPollEntryList();
+		// sortirani po broju glasova
+		entries = entries.stream().sorted().collect(Collectors.toList());
+		
+		req.setAttribute("pollResults", entries);
+		
+		req.getRequestDispatcher("/WEB-INF/pages/glasanjeRez.jsp").forward(req, resp); 
+	}
+	
+}
