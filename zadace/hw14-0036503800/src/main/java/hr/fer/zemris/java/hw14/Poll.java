3326f9232aaa0de97942dd798d640714c96f41df
kuzzmak <kuzminski.antonio@gmail.com>
2020-06-16 22:24:40 +0200

    one poll

---
+++ zadace/hw14-0036503800/src/main/java/hr/fer/zemris/java/hw14/Poll.java
@@ -0,0 +1,36 @@
+package hr.fer.zemris.java.hw14;
+
+public class Poll {
+	
+	private long id;
+	private String title;
+	private String message;
+	
+	public Poll() {
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+}
