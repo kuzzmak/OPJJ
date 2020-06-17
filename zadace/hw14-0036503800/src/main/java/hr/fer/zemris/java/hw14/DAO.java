1254e941f630c5a2d95f1f97f038456f1be32a2f
kuzzmak <kuzminski.antonio@gmail.com>
2020-06-17 13:36:27 +0200

    updated

--- zadace/hw14-0036503800/src/main/java/hr/fer/zemris/java/hw14/DAO.java
+++ zadace/hw14-0036503800/src/main/java/hr/fer/zemris/java/hw14/DAO.java
@@ -8,21 +8,40 @@ import java.util.List;
  *
  */
 public interface DAO {
+	
+	/**
+	 * Dohvaća listu mogućih anketa za glasanje.
+	 * 
+	 * @return lista anketa za glasanje
+	 * @throws DAOException ukoliko je došlo do greške
+	 */
+	public List<Poll> getPollList() throws DAOException;
 
 	/**
 	 * Dohvaća sve postojeće unose u bazi za glasanje.
 	 * 
+	 * @param pollID identifikacijski broj ankete
 	 * @return listu unosa za glasanje
 	 * @throws DAOException u slučaju pogreške
 	 */
-	public List<PollEntry> getPollEntryList() throws DAOException;
+	public List<PollEntry> getPollEntryList(Long pollID) throws DAOException;
 	
 	/**
-	 * Dohvaća Unos za zadani id. Ako unos ne postoji, vraća <code>null</code>.
-	 * @param id
-	 * @return
-	 * @throws DAOException
+	 * Dohvat određenog unosa u anketi za glasanje.
+	 * 
+	 * @param pollID identifikacijski broj ankete
+	 * @param entryID identifikacijski broj unosa
+	 * @return objekt tipa {@code PollEntry}
+	 * @throws DAOException ukoliko je došlo do greške
 	 */
-	public Unos dohvatiUnos(long id) throws DAOException;
+	public PollEntry getPollEntryById(Long pollID, Long entryID) throws DAOException;
+	
+	/**
+	 * Metoda za glasanje na određenoj anketi.
+	 * 
+	 * @param entryID identifikacijski broj unosa za koji se glasalo
+	 * @throws DAOException ukoliko je došlo do greške
+	 */
+	public void voteFor(PollEntry entry) throws DAOException;
 	
 }
