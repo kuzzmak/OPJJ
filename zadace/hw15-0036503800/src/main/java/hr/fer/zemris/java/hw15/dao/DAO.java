0e6b40e13dee2e10be41d9d61f00aec28011da89
kuzzmak <kuzminski.antonio@gmail.com>
2020-06-26 01:47:41 +0200

    dao interface

---
+++ zadace/hw15-0036503800/src/main/java/hr/fer/zemris/java/hw15/dao/DAO.java
@@ -0,0 +1,20 @@
+package hr.fer.zemris.java.hw15.dao;
+
+import hr.fer.zemris.java.hw15.forms.BlogUserForm;
+import hr.fer.zemris.java.hw15.model.BlogEntry;
+
+public interface DAO {
+
+	/**
+	 * Dohvaća entry sa zadanim <code>id</code>-em. Ako takav entry ne postoji,
+	 * vraća <code>null</code>.
+	 * 
+	 * @param id ključ zapisa
+	 * @return entry ili <code>null</code> ako entry ne postoji
+	 * @throws DAOException ako dođe do pogreške pri dohvatu podataka
+	 */
+	public BlogEntry getBlogEntry(Long id) throws DAOException;
+	
+	public void registerNewUser(BlogUserForm buf) throws DAOException;
+	
+}
