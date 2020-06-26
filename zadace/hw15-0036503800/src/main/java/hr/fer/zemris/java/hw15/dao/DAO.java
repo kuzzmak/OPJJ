dbe13c3649e2d1a8e6fb0a96bf0ceecf86c83863
kuzzmak <kuzminski.antonio@gmail.com>
2020-06-26 23:24:21 +0200

    added getter for registered users

--- zadace/hw15-0036503800/src/main/java/hr/fer/zemris/java/hw15/dao/DAO.java
+++ zadace/hw15-0036503800/src/main/java/hr/fer/zemris/java/hw15/dao/DAO.java
@@ -1,7 +1,10 @@
 package hr.fer.zemris.java.hw15.dao;
 
+import java.util.List;
+
 import hr.fer.zemris.java.hw15.forms.BlogUserForm;
 import hr.fer.zemris.java.hw15.model.BlogEntry;
+import hr.fer.zemris.java.hw15.model.BlogUser;
 
 public interface DAO {
 
@@ -23,4 +26,20 @@ public interface DAO {
 	 */
 	public void registerNewUser(BlogUserForm buf) throws DAOException;
 	
+	/**
+	 * Dohvaća korisnika iz baze prema predanom korisničkom imenu.
+	 * 
+	 * @param username korisničko ime korisnika
+	 * @return objekt tipa {@code BlogUser}
+	 * @throws DAOException ukoliko je došlo do pogreške
+	 */
+	public BlogUser findUserByUsername(String username) throws DAOException;
+	
+	/**
+	 * Dohvaća sve registrirane korisnike iz baze.
+	 * 
+	 * @return lista registriranih korisnika
+	 * @throws DAOException ukoliko je došlo do greške
+	 */
+	public List<BlogUser> getAllUsers() throws DAOException;
 }
