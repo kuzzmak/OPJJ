12ee647fbfc0ba80c8fac797d6127b744814ca9e
kuzzmak <kuzminski.antonio@gmail.com>
2020-06-26 17:18:07 +0200

    servlet for login

---
+++ zadace/hw15-0036503800/src/main/java/hr/web/adresar/servleti/LoginServlet.java
@@ -0,0 +1,37 @@
+package hr.web.adresar.servleti;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import hr.fer.zemris.java.hw15.forms.BlogUserForm;
+import hr.fer.zemris.java.hw15.model.BlogUser;
+
+/**
+ * Servlet koji obrađuje prijavu korisnika.
+ * 
+ * @author Antonio Kuzminski
+ *
+ */
+@WebServlet(name="login", urlPatterns={"/servleti/login"})
+public class LoginServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		BlogUserForm buf = new BlogUserForm();
+		buf.fromHttpRequest(req);
+		
+		BlogUser bu = buf.getBlogUser();
+		
+		
+		System.out.println(buf);
+	}
+
+}
