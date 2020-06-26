e10f482aadaeab653dc00810fc390eb54498bbcb
kuzzmak <kuzminski.antonio@gmail.com>
2020-06-26 17:23:58 +0200

    main servlet

---
+++ zadace/hw15-0036503800/src/main/java/hr/web/adresar/servleti/Main.java
@@ -0,0 +1,22 @@
+package hr.web.adresar.servleti;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name="main", urlPatterns={"/servleti/main"})
+public class Main extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		req.getRequestDispatcher("/WEB-INF/pages/Main.jsp").forward(req, resp);
+	}
+}
