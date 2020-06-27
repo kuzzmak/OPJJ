0ce050e2079cbb450779aadf828a5296f0788819
kuzzmak <kuzminski.antonio@gmail.com>
2020-06-27 15:33:57 +0200

    servlet for commenting and viewing comments

---
+++ zadace/hw15-0036503800/src/main/java/hr/fer/zemris/java/hw15/servlets/CommentServlet.java
@@ -0,0 +1,36 @@
+package hr.fer.zemris.java.hw15.servlets;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import hr.fer.zemris.java.hw15.dao.DAOProvider;
+import hr.fer.zemris.java.hw15.model.BlogComment;
+
+/**
+ * Servlet koji dohvaća komentare objave te ih sprema u kontekst sjednice.
+ * 
+ * @author Antonio Kuzminski
+ *
+ */
+@WebServlet(name="comments", urlPatterns={"/servleti/comments"})
+public class CommentServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		Long blogId = Long.valueOf(req.getParameter("blogId"));
+		List<BlogComment> blogEntryComments = DAOProvider.getDAO().getBlogEntryComments(blogId);
+		
+		req.getSession().setAttribute("blogEntryComments", blogEntryComments);
+		req.getRequestDispatcher("/WEB-INF/pages/Comments.jsp").forward(req, resp);
+	}
+	
+}
