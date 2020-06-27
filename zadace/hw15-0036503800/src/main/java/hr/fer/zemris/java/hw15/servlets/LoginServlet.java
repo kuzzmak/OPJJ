f049fb7ac9b34a0bf60471cf2c3556829a081711
kuzzmak <kuzminski.antonio@gmail.com>
2020-06-27 15:33:04 +0200

    bug fixes

--- zadace/hw15-0036503800/src/main/java/hr/fer/zemris/java/hw15/servlets/LoginServlet.java
+++ zadace/hw15-0036503800/src/main/java/hr/fer/zemris/java/hw15/servlets/LoginServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import hr.fer.zemris.java.hw15.dao.DAOProvider;
 import hr.fer.zemris.java.hw15.forms.BlogUserForm;
 import hr.fer.zemris.java.hw15.model.BlogUser;
 
@@ -21,6 +22,11 @@ import hr.fer.zemris.java.hw15.model.BlogUser;
 public class LoginServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.sendRedirect(req.getServletContext().getContextPath() + "/servleti/main");
+	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -30,8 +36,22 @@ public class LoginServlet extends HttpServlet {
 		
 		BlogUser bu = buf.getBlogUser();
 		
+		// korisnik iz baze s korisničkim imenom od "bu"
+		BlogUser buInDatabase = DAOProvider.getDAO().findUserByUsername(bu.getNick());
 		
-		System.out.println(buf);
+		// ako nije pronađen korisnik s takvim korisničkim imenom ili se sažetci lozinka ne slažu,
+		// preusmjerava se opet na početnu stranicu
+		if(buInDatabase == null || !buInDatabase.getPasswordHash().equals(bu.getPasswordHash())) {
+			resp.sendRedirect(req.getServletContext().getContextPath() + "/servleti/login");
+		}else {
+			
+			req.getSession().setAttribute("current.user.id", buInDatabase.getId());
+			req.getSession().setAttribute("current.user.nick", buInDatabase.getNick());
+			req.getSession().setAttribute("current.user.fn", buInDatabase.getFirstName());
+			req.getSession().setAttribute("current.user.ln", buInDatabase.getLastName());
+			
+			resp.sendRedirect(req.getContextPath() + "/servleti/author?nick=" + buInDatabase.getNick());
+		}
 	}
 
 }
