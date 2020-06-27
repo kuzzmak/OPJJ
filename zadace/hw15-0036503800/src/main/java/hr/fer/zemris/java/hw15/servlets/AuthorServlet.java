2925a9750d092200d38527a3e2d4f8e3e5ab495f
kuzzmak <kuzminski.antonio@gmail.com>
2020-06-27 15:34:50 +0200

    updated

---
+++ zadace/hw15-0036503800/src/main/java/hr/fer/zemris/java/hw15/servlets/AuthorServlet.java
@@ -0,0 +1,22 @@
+package hr.fer.zemris.java.hw15.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name="author", urlPatterns={"/servleti/author"})
+public class AuthorServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		req.getRequestDispatcher("/WEB-INF/pages/Author.jsp").forward(req, resp);
+	}
+	
+}
